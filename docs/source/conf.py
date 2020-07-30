b774019a8fb4fb058dc882711ca1d8fc12772d36
haipinglu <haipinglu@users.noreply.github.com>
2020-07-30 23:44:02 +0100

    Rename doc headings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,6 +37,8 @@ extensions = ['sphinx_rtd_theme',
 'sphinx.ext.autosummary',
 'sphinx.ext.intersphinx',
 'nbsphinx',
+'nbsphinx_link',
+'sphinx.ext.mathjax',
 'sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
@@ -45,7 +47,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 
 # -- Options for HTML output -------------------------------------------------
