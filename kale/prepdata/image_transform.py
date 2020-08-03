d8fc064087b07b21093ae43b3c00c0b9f30ba426
haipinglu <haipinglu@users.noreply.github.com>
2020-08-04 00:30:02 +0100

    Renamed im_transform and changed augmentation to an option for cifar and office

--- kale/prepdata/image_transform.py
+++ kale/prepdata/image_transform.py
@@ -1,12 +1,20 @@
-"""Preprocessing of image datasets, i.e. transforms
-Modified by Haiping Lu from
+"""
+Preprocessing of image datasets, i.e., transforms, from
 https://github.com/criteo-research/pytorch-ada/blob/master/adalib/ada/datasets/preprocessing.py
 """
 
 import torchvision.transforms as transforms
 
 
-def get_transform(kind):
+def get_transform(kind, augment=False):
+    """
+    Define transforms (for commonly used datasets)
+
+    Args:
+        kind ([type]): the dataset (transformation) name
+        augment (bool, optional): whether to do data augmentation (random crop and flipping). Defaults to False. (Not implemented for digits yet.)
+
+    """
     if kind == "mnist32":
         transform = transforms.Compose(
             [
@@ -59,21 +67,41 @@ def get_transform(kind):
                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
             ]
         )
-    elif kind == "cifar_train":
+    elif kind == "cifar":
+        if augment:
+            transform_aug=transforms.Compose(
+                [
+                    transforms.RandomCrop(32, padding=4), 
+                    transforms.RandomHorizontalFlip(),
+                ]
+            )
+        else:
+            transform_aug=transforms.Compose([])           
         transform = transforms.Compose(
             [
-               transforms.RandomCrop(32, padding=4), 
-               transforms.RandomHorizontalFlip(),
+               transform_aug,                  
                transforms.ToTensor(),
                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
             ]
         )        
     elif kind == "office":
+        if augment:
+            transform_aug=transforms.Compose(
+                [
+                    transforms.Resize(256),
+                    transforms.RandomResizedCrop(224),
+                    transforms.RandomHorizontalFlip()
+                ]
+            )
+        else:
+            transform_aug=transforms.Compose(
+                [
+                    transforms.Resize(256)
+                ]    
+            )        
         transform = transforms.Compose(
             [
-                transforms.Resize(256),
-                transforms.RandomResizedCrop(224),
-                transforms.RandomHorizontalFlip(),
+                transform_aug,
                 transforms.ToTensor(),
                 transforms.Normalize(
                     mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
