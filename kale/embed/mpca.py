691e143f499426dec615df5180acf7984c317284
Shuo Zhou <szhou20@sheffield.ac.uk>
2020-08-12 00:06:08 +0100

    update mpca, cmr docs

--- kale/embed/mpca.py
+++ kale/embed/mpca.py
@@ -19,18 +19,28 @@ class MPCA(BaseEstimator, TransformerMixin):
     def __init__(self, variance_explained=0.97, max_iter=1):
         """MPCA implementation compatible with sickit-learn
 
-        Parameters
-        ----------
-        variance_explained : float, optional
-            ration of variance to keep (between 0 and 1), by default 0.97
-        max_iter : int, optional
-            max number of iteration, by default 1
+        Args:
+            variance_explained (float, optional): ration of variance to keep 
+                (between 0 and 1). Defaults to 0.97.
+            max_iter (int, optional): max number of iteration. Defaults to 1.
 
-        ----------
         Attributes:
             tPs: list of projection matrices
+
+        Examples:
+        >>> import numpy as np
+        >>> from kale.embed.mpca import MPCA
+        >>> X = np.random.random((20, 25, 20, 40))
+        >>> X.shape
+        (20, 25, 20, 40)
+        >>> mpca = MPCA(variance_explained=0.9)
+        >>> X_transformed = mpca.fit_transform(X)
+        >>> X_transformed.shape
+        (18, 23, 18, 40)
+        >>> X_inverse = mpca.inverse_transform(X_transformed)
+        >>> X_inverse.shape
+        (20, 25, 20, 40)
         """
-        
         self.var_exp = variance_explained
         self.max_iter = max_iter
         # self.Xmean = None
@@ -40,16 +50,12 @@ class MPCA(BaseEstimator, TransformerMixin):
     def fit(self, X):
         """Fit the model with X
 
-        Parameters
-        ----------
-        X : ndarray
-            input data, shape (I1, I2, ..., n_samples), where
-            n_samples is the number of samples, I1, I2 ... are 
-            the dimensions of corresponding mode (1, 2, ...), 
-            respectively.
+        Args:
+            X (ndarray): input data, shape (I1, I2, ..., n_samples), where
+            n_samples is the number of samples, I1, I2 ... are the dimensions 
+            of corresponding mode (1, 2, ...), respectively.
 
-        Returns
-        -------
+        Returns:
             self
         """
         dim_in = X.shape
@@ -106,15 +112,11 @@ class MPCA(BaseEstimator, TransformerMixin):
     def transform(self, X):
         """Perform dimension reduction on X
 
-        Parameters
-        ----------
-        X : ndarray
-            shape (I1, I2, ..., n_samples)
+        Args:
+            X (ndarray): shape (I1, I2, ..., n_samples)
 
-        Returns
-        -------
-        ndarray
-            Transformed data, shape (i1, i2, ..., n_samples)
+        Returns:
+            ndarray: Transformed data, shape (i1, i2, ..., n_samples)
         """
         n_spl = X.shape[-1]
         for i in range(n_spl):
@@ -125,37 +127,40 @@ class MPCA(BaseEstimator, TransformerMixin):
         """Transform data in the shape of reduced dimension 
         back to the original shape
 
-        Parameters
-        ----------
-        X : ndarray
-            shape (i1, i2, ..., n_samples), where i1, i2, ... 
+        Args:
+            X (ndarray): shape (i1, i2, ..., n_samples), where i1, i2, ... 
             are the reduced dimensions of of corresponding mode
             (1, 2, ...), respectively.
 
-        Returns
-        -------
-        ndarray
-            Data in original shape, shape (I1, I2, ..., n_samples)
+        Returns:
+            ndarray: Data in original shape, shape (I1, I2, ..., n_samples)
         """
+        
         return multi_mode_dot(X, self.tPs, modes=[m for m in range(self.n_dim - 1)], transpose=True)
 
 
 def MPCA_(X, variance_explained=0.97, max_iter=1):
-    """[summary]
+    """MPCA implementation as a function
 
-    Parameters
-    ----------
-    X : ndarray 
-        training data, shape (I1, I2, ..., n_samples)
-    variance_explained : float, optional
-        ration of variance to keep (between 0 and 1), by default 0.97, by default 0.97
-    max_iter : int, optional
-        max number of iteration, by default 1
+    Args:
+        X (ndarray): training data, shape (I1, I2, ..., n_samples)
+        variance_explained (float, optional): ration of variance to keep (between 0 and 1). Defaults to 0.97.
+        max_iter (int, optional): max number of iteration. Defaults to 1.
 
-    Returns
-    -------
-    list
-        tPs: list of projection matrices
+    Returns:
+        list: tPs: list of projection matrices
+    
+    Examples:
+    >>> import numpy as np
+    >>> from tensorly.tenalg import multi_mode_dot
+    >>> from kale.embed.mpca import MPCA_
+    >>> X = np.random.random((20, 25, 20, 40))
+    >>> X.shape
+    (20, 25, 20, 40)
+    >>> tPs = MPCA_(X, variance_explained=0.9)
+    >>> X_transformed = multi_mode_dot(X, tPs, modes=[0, 1, 2])
+    >>> X_transformed.shape
+    (18, 23, 18, 40)
     """
     dim_in = X.shape
     n_spl = dim_in[-1]
@@ -208,5 +213,3 @@ def MPCA_(X, variance_explained=0.97, max_iter=1):
             tPs[i] = tPs[i][:, :cums[i]].T
 
     return tPs
-
-
--- kale/prepdata/prep_cmr.py
+++ kale/prepdata/prep_cmr.py
@@ -11,19 +11,13 @@ from skimage import exposure, transform
 def regMRI(data, reg_df, reg_id=1):
     """Align CMR images towards a target sample
 
-    Parameters
-    ----------
-    data : ndarray
-        input data, shape (dim1, dim2, n_slice, n_subject)
-    reg_df : dataframe
-        landmark locations for registration, shape (n_subject, 2 + n_landmark * 2)
-    reg_id : int, optional
-        index of subject used as target sample, by default 1
+    Args:
+        data (ndarray): input data, shape (dim1, dim2, n_slice, n_subject)
+        reg_df (dataframe): landmark locations for registration, shape (n_subject, 2 + n_landmark * 2)
+        reg_id (int, optional): index of subject used as target sample. Defaults to 1.
 
-    Returns
-    -------
-    ndarray
-        Processed data, shape (dim1, dim2, n_slice, n_subject)
+    Returns:
+        ndarray: Processed data, shape (dim1, dim2, n_slice, n_subject)
     """
     n_sample = data.shape[-1]
     if n_sample != reg_df.shape[0]:
@@ -62,17 +56,12 @@ def regMRI(data, reg_df, reg_id=1):
 def rescale_cmr(data, scale=16):
     """Rescaling CMR data
 
-    Parameters
-    ----------
-    data : array-like
-        CMR data, shape (dim1, dim2, n_slice, n_subject)
-    scale : int, optional
-        Scale factor, by default 16
+    Args:
+        data (ndarray): CMR data, shape (dim1, dim2, n_slice, n_subject)
+        scale (int, optional): Scale factor. Defaults to 16.
 
-    Returns
-    -------
-    [type]
-        [description]
+    Returns:
+        ndarray: rescaled CMR images, shape (dim1 / scale, dim2 / scale, n_slice, n_subject)
     """
     n_sub = data.shape[-1]
     n_time = data.shape[-2]
@@ -95,15 +84,11 @@ def rescale_cmr(data, scale=16):
 def mat2gray(A):
     """Convert an image to gray scale
 
-    Parameters
-    ----------
-    A : ndarray
-        input image, shape (dim1, dim2)
+    Args:
+        A (ndarray): input image, shape (dim1, dim2)
 
-    Returns
-    -------
-    ndarray
-        image where all values are between 0 and 1
+    Returns:
+        ndarray: image where all values are between 0 and 1
     """
     amin = np.amin(A)
     amax = np.amax(A)
@@ -114,19 +99,13 @@ def mat2gray(A):
 def preproc(data, mask, level=1):
     """Preprocessing CMR image data
 
-    Parameters
-    ----------
-    data : ndarray
-        iuput image data, shape (dim1, dim2, n_slice, n_subject)
-    mask : ndarray
-        mask, shape (dim1, dim2)
-    level : int, optional
-        preprocessing level, by default 1
+    Args:
+        data (ndarray): iuput image data, shape (dim1, dim2, n_slice, n_subject)
+        mask (ndarray): mask, shape (dim1, dim2)
+        level (int, optional): preprocessing level, by default 1. Defaults to 1.
 
-    Returns
-    -------
-    ndarray
-        preprocessed data, shape (dim1, dim2, n_slice, n_subject)
+    Returns:
+        ndarray: preprocessed data, shape (dim1, dim2, n_slice, n_subject)
     """
     n_sub = data.shape[-1]
     n_time = data.shape[-2]
@@ -149,17 +128,12 @@ def preproc(data, mask, level=1):
 def scale_cmr_mask(mask, scale):
     """Rescale mask
 
-    Parameters
-    ----------
-    mask : ndarray
-        mask matrix, shape (dim1, dim2)
-    scale : int
-        Scale factors, e.g. the output will be 1/4 of given data if scale=4
+    Args:
+        mask (ndarray): mask matrix, shape (dim1, dim2)
+        scale (int): scale factors, e.g. the output will be 1/4 of given data if scale=4
 
-    Returns
-    -------
-    ndarray
-        Scaled mask
+    Returns:
+        ndarray: rescaled mask
     """
     mask = transform.rescale(mask.astype('bool_'), 1/scale, anti_aliasing=False)
     # change mask dtype to bool to ensure the output values are 0 and 1
@@ -176,25 +150,16 @@ def scale_cmr_mask(mask, scale):
 def cmr_proc(data_path, db, scale, mask_path, mask_id, level):
     """Load image data, masks and perform preprocessing
 
-    Parameters
-    ----------
-    data_path : str
-        full path to data file
-    db : int
-        data base id, 2 for 4 chamber and 17 for short axis
-    scale : int
-        sclae factor, e.g. the output will be 1/4 of given data if scale=4
-    mask_path : str
-        full path to image masks
-    mask_id : int
-        index of mask used for preprocessing
-    level : int
-        preprocessing level
+    Args:
+        data_path (str): full path to data file
+        db (int): data base id, 2 for 4 chamber and 17 for short axis
+        scale (int): sclae factor, e.g. the output will be 1/4 of given data if scale=4
+        mask_path (str): full path to image masks
+        mask_id (int): index of mask used for preprocessing
+        level (int): preprocessing level
 
-    Returns
-    -------
-    ndarray
-        preprocessed data
+    Returns:
+        ndarray: preprocessed data
     """
     print('Preprocssing Data Scale: 1/%s, Mask ID: %s, Processing level: %s'
           % (scale, mask_id, level))
