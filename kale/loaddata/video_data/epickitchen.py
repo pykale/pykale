6e9ed58d60044a8c1808704cf98ff3a1c2c57feb
Xianyuan <xianyuan.liu@sheffield.ac.uk>
2020-11-16 22:05:34 +0000

    Update epickitchen.py

--- kale/loaddata/video_data/epickitchen.py
+++ kale/loaddata/video_data/epickitchen.py
@@ -9,6 +9,11 @@ import pickle
 class EPIC(BasicVideoDataset):
 
     def __getitem__(self, item):
+        """
+        Get images from each EPIC-Kitchen action video sample and labels for data loader.
+        Randomly get a 16-frame sample from each video
+        Follow this paper. (https://ieeexplore.ieee.org/document/9156981)
+        """
         self.vid, self.start_frame, self.end_frame, self.label = self.data[item]
         self.rand_start_frame = random.randint(self.start_frame, self.end_frame - (self.window_len + 1))
         self.img_path = os.path.join(self.data_path, self.mode, self.dataset, self.vid)
@@ -23,14 +28,16 @@ class EPIC(BasicVideoDataset):
         return self.video_to_tensor(imgs), self.label
 
     def make_dataset(self):
+        """
+        Load data from the EPIC-Kitchen list file.
+        Because the original list files are not the same, inherit from class BasicVideoDataset and be modified.
+        """
         data = []
         i = 0
         with open(self.list_path, 'rb') as input_file:
             input_file = pickle.load(input_file)
-            # print(type(input_file))
             for line in input_file.values:
                 if line[1] in ['P01', 'P08', 'P22']:
-                    # Select class 'put', 'take', 'open', 'close', 'wash', 'cut', 'mix', 'pour', 'put down', 'wipe'...
                     if 0 <= line[9] < self.n_classes:
                         if line[7] - line[6] + 1 >= self.window_len:
                             label = line[9]
