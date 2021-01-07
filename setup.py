6702103cedb71410f9f2cd91f9fdc73b655afa80
haipinglu <haipinglu@users.noreply.github.com>
2021-01-07 12:41:17 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,65 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python3
 
-#To do
+import io
+import os
+import re
+
+from setuptools import find_packages, setup
+
+
+# Get version
+def read(*names, **kwargs):
+    with io.open(os.path.join(os.path.dirname(__file__), *names), encoding=kwargs.get("encoding", "utf8")) as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+readme = open("README.md").read()
+version = find_version("pykale", "__init__.py")
+
+
+torch_min = "1.7"
+install_requires = [">=".join(["torch", torch_min]), "scikit-learn"]
+# if recent dev version of PyTorch is installed, no need to install stable
+try:
+    import torch
+
+    if torch.__version__ >= torch_min:
+        install_requires = []
+except ImportError:
+    pass
+
+
+# Run the setup
+setup(
+    name="pykale",
+    version=version,
+    description="A Python library for knowledge-aware machine learning",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="The PyKale team",
+    url="https://pykale.readthedocs.io",
+    author_email="pykale-group@sheffield.ac.uk",
+    project_urls={
+        "Documentation": "https://pykale.readthedocs.io",
+        "Source": "https://github.com/pykale/pykale",
+    },
+    license="MIT",
+    classifiers=["Development Status :: 2 - Pre-Alpha", "Programming Language :: Python :: 3"],
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=install_requires,
+    extras_require={
+        "dev": ["black", "twine", "pre-commit"],
+        "docs": ["ipython", "ipykernel", "sphinx<3.0.0", "sphinx_rtd_theme", "nbsphinx", "m2r"],
+        "examples": ["ipython", "jupyter", "matplotlib", "numpy", "torchvision", "tqdm"],
+        "test": ["flake8", "flake8-print", "pytest", "nbval"],
+    },
+)
