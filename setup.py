f681174bdcb7e67af3202d616b27a9bff0461466
haipinglu <haipinglu@users.noreply.github.com>
2021-01-31 12:11:24 +0000

    switch to two requirements*.txt for neater setup.py

--- setup.py
+++ setup.py
@@ -4,59 +4,13 @@ from os import path
 
 from setuptools import find_packages, setup
 
-# Not all packages have a min-version specified, which is not uncommon. Specify when needed (e.g. errors).
-# Install PyTorch from the official website to match your hardware.
-# To work on graphs, install torch-geometric following the official instructions (e.g. below):
-# python -m pip install torch-cluster torch-scatter torch-sparse torch-spline
+# Core kale API dependencies
+with open("requirements.txt", encoding='utf-8') as f:
+    requirements = f.read().splitlines()
 
-# Core kale API dependencies, update docs/requirements.txt too
-requirements = [
-    "numpy>=1.18.0",
-    "scikit-learn",
-    "scikit-image",
-    "torch>=1.7.0",
-    "torchvision>=0.8.1",
-    "pytorch-lightning",
-    "tensorly",
-]
-
-# Dependencies for examples/tutorials and development
-extras = {
-    "docs": [
-        "ipython",
-        "ipykernel",
-        "sphinx",
-        "sphinx_rtd_theme",
-        "nbsphinx",
-        "m2r",
-    ],
-    "check (linting)": [
-        "black",
-        "flake8",
-        "flake8-print",
-        "isort",
-        "mypy",
-        "pre-commit",
-    ],
-    "utils": [
-        "matplotlib",
-        "torchsummary>=1.5.0",
-        "tqdm>=4.1.0",
-        "yacs>=0.1.7",
-    ],
-    "test": [
-        "nbval",
-        "pytest",
-    ],
-    "publish": [
-        "twine",
-    ],
-},
-
-# Alternative choice but not flexible in separating essential and extra --> NOT adopted
-# read the contents of requirements.txt
-# with open("requirements.txt", encoding='utf-8') as f:
-#     requirements = f.read().splitlines()
+# Additional dependencies for examples/tutorials and development
+with open("requirements-extras.txt", encoding='utf-8') as f:
+    extra_requirements = f.read().splitlines()
 
 # Get __version__ from __init__.py in kale
 version_file = path.join("kale", "__init__.py")
@@ -85,7 +39,7 @@ setup(
     packages=find_packages(exclude=("docs", "examples", "tests")),
     python_requires=">=3.6",
     install_requires=requirements,
-    extras_require=extras,
+    extras_require={'extras': extra_requirements},
     setup_requires=['setuptools>=38.6.0'],
     license="MIT",
     keywords="machine learning, pytorch, deep learning, multimodal learning, transfer learning",
