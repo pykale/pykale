a44c40112ff5628ea8c9b1c96027ca725d74d42d
Xianyuan <xianyuan.liu@sheffield.ac.uk>
2021-02-13 23:29:04 +0000

    Change output V_tar_acc to .2f

--- kale/utils/csv_logger.py
+++ kale/utils/csv_logger.py
@@ -99,7 +99,7 @@ def setup_logger(train_params, output_dir, method_name, seed):
     path_method_name = re.sub(r"[^-/\w\.]", "_", method_name)
     full_checkpoint_dir = os.path.join(checkpoint_dir, path_method_name, f"seed_{seed}")
     checkpoint_callback = ModelCheckpoint(
-        filepath=os.path.join(full_checkpoint_dir, "{epoch}-{V_target_acc:.2f}"),
+        filepath=os.path.join(full_checkpoint_dir, "{epoch}-{V_target_acc:.4f}"),
         save_top_k=3,
         monitor="V_target_acc",
         mode="max",
