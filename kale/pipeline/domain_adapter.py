ddc2d6e7a9a0f9d8b32625ead3ada6d14fe8e6b9
Xianyuan <xianyuan.liu@sheffield.ac.uk>
2021-02-15 23:32:37 +0000

    Change lr_fact back

--- kale/pipeline/domain_adapter.py
+++ kale/pipeline/domain_adapter.py
@@ -290,8 +290,7 @@ class BaseAdaptTrainer(pl.LightningModule):
             self._grow_fact = 2.0 / (1.0 + np.exp(-10 * p)) - 1
 
             if self._adapt_lr:
-                # self._lr_fact = 1.0 / ((1.0 + 10 * p) ** 0.75)
-                self._lr_fact = 0.5
+                self._lr_fact = 1.0 / ((1.0 + 10 * p) ** 0.75)
 
         if self._adapt_lambda:
             self.lamb_da = self._init_lambda * self._grow_fact
