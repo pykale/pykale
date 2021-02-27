fd784e8eca55bb18af25b719e4e18482f8b3d482
Xianyuan <xianyuan.liu@sheffield.ac.uk>
2021-02-27 02:43:44 +0000

    Active random crop

--- kale/prepdata/video_transform.py
+++ kale/prepdata/video_transform.py
@@ -19,8 +19,8 @@ def get_transform(kind, image_modality):
                     [
                         ImglistToTensor(),
                         transforms.Resize(size=256),
-                        transforms.CenterCrop(size=112),
-                        # transforms.RandomCrop(size=224),
+                        # transforms.CenterCrop(size=112),
+                        transforms.RandomCrop(size=112),
                         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
                         TensorPermute(),
                     ]
