bafb933cb6db75f836addc7c801826a94bee3b11
bobturneruk <r.d.turner@sheffield.ac.uk>
2021-03-27 09:43:34 +0000

    bassic logger test with teardown

--- tests/utils/test_logger.py
+++ tests/utils/test_logger.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import pytest
 
@@ -7,7 +8,17 @@ from kale.utils import logger
 
 @pytest.fixture
 def testing_logger():
-    return logger.construct_logger("test", "./")
+    save_dir = "./"
+    testing_logger = logger.construct_logger("test", save_dir)
+    yield testing_logger
+
+    # Teardown
+    filehandler = testing_logger.handlers[0]
+    log_file_name = filehandler.baseFilename
+    gitdiff_file_name = os.path.join(save_dir, "gitdiff.patch")
+    filehandler.close()
+    os.remove(log_file_name)
+    os.remove(gitdiff_file_name)
 
 
 def test_logger_type(testing_logger):
