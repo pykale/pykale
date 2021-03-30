8d157a37620e2716a34b17d0492ed2122480763e
bobturneruk <r.d.turner@sheffield.ac.uk>
2021-03-30 12:27:45 +0100

    check log files created

--- tests/utils/test_logger.py
+++ tests/utils/test_logger.py
@@ -6,7 +6,7 @@ import pytest
 from kale.utils import logger
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def testing_logger():
     save_dir = "./"
     testing_logger = logger.construct_logger("test", save_dir)
@@ -27,6 +27,19 @@ def testing_logger():
     os.remove(gitdiff_file_name)
 
 
+@pytest.fixture
+def log_file_name(testing_logger):
+    filehandler = testing_logger.handlers[0]
+    yield filehandler.baseFilename
+
+
+@pytest.fixture
+def gitdiff_file_name(log_file_name):
+    [folder, file] = os.path.split(log_file_name)
+    file_core = os.path.splitext(file)[0]
+    yield os.path.join(folder, file_core + ".gitdiff.patch")
+
+
 def test_out_file_core():
     out_file_eg = logger.out_file_core()
     assert isinstance(out_file_eg, str)
@@ -34,3 +47,11 @@ def test_out_file_core():
 
 def test_logger_type(testing_logger):
     assert isinstance(testing_logger, logging.Logger)
+
+
+def test_log_file_exists(log_file_name):
+    assert os.path.isfile(log_file_name)
+
+
+def test_gitdiff_file_exists(gitdiff_file_name):
+    assert os.path.isfile(gitdiff_file_name)
