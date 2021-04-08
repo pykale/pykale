f4b2fa6c5c31d96b9a1c41856dfb339295c294d0
White <pbai2@sheffield.ac.uk>
2021-04-08 17:02:16 +0800

    Sort documented members by source order

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -80,6 +80,9 @@ exclude_patterns = ["_build", "**.ipynb_checkpoints"]
 # html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
 
+# Automatically documented members are sorted by source order
+autodoc_member_order = 'bysource'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
