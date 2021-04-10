afe55e46444d38bf25d135afa6c767489f20d4ba
Shuo Zhou <szhou20@sheffield.ac.uk>
2021-04-10 13:50:20 +0800

    Update test_mpca_trainer.py

--- tests/pipeline/test_mpca_trainer.py
+++ tests/pipeline/test_mpca_trainer.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 from numpy import testing
 from scipy.io import loadmat
+from sklearn.metrics import accuracy_score, roc_auc_score
 
 from kale.pipeline.mpca_trainer import MPCATrainer
 
@@ -11,28 +12,24 @@ x = x[:20, :]
 y = gait["gnd"][:20]
 
 CLASSIFIERS = ["svc", "lr"]
-CLASSIFIER_PARAMS = ["auto", {"C": 1}]
-MPCA_PARAMS = [None, {"var_ratio": 0.9, "return_vector": True}]
-N_FEATURES = [None, 100]
-SEARCH_PARAMS = [None, {'cv': 3}]
+PARAMS = [{"classifier_params": "auto", "mpca_params": None, "n_features": None, "search_params": None},
+          {"classifier_params": {"C": 1}, "mpca_params": {"var_ratio": 0.9, "return_vector": True},
+           "n_features": 100, "search_params": {'cv': 3}}]
 
 
 @pytest.mark.parametrize("classifier", CLASSIFIERS)
-@pytest.mark.parametrize("classifier_params", CLASSIFIER_PARAMS)
-@pytest.mark.parametrize("mpca_params", MPCA_PARAMS)
-@pytest.mark.parametrize("n_features", N_FEATURES)
-@pytest.mark.parametrize("search_params", SEARCH_PARAMS)
-def test_mpca_trainer(classifier, classifier_params, mpca_params, n_features, search_params):
-    trainer = MPCATrainer(classifier=classifier, classifier_params=classifier_params, mpca_params=mpca_params,
-                          n_features=n_features, search_params=search_params)
+@pytest.mark.parametrize("params", PARAMS)
+def test_mpca_trainer(classifier, params):
+    trainer = MPCATrainer(classifier=classifier, **params)
     trainer.fit(x, y)
     y_pred = trainer.predict(x)
     testing.assert_equal(np.unique(y), np.unique(y_pred))
+    assert accuracy_score(y, y_pred) >= 0.8
 
     y_proba = trainer.predict_proba(x)
     assert np.max(y_proba) <= 1.0
     assert np.min(y_proba) >= 0.0
+    assert roc_auc_score(y, y_proba) >= 0.8
 
     y_dec_score = trainer.decision_function(x)
-    testing.assert_equal(y_dec_score.shape, (x.shape[0],))
-
+    assert roc_auc_score(y, y_dec_score) >= 0.8
