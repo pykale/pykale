e8cf112f89e09f23b67e875d8e29e84402909318
Xianyuan <xianyuan.liu@sheffield.ac.uk>
2021-04-18 00:55:44 +0100

    Update video_se_res3d.py

--- kale/embed/video_se_res3d.py
+++ kale/embed/video_se_res3d.py
@@ -1,17 +1,14 @@
-from torchvision.models.utils import load_state_dict_from_url
+# =============================================================================
+# Author: Xianyuan Liu, xianyuan.liu@sheffield.ac.uk
+#         Haiping Lu, h.lu@sheffield.ac.uk or hplu@ieee.org
+# =============================================================================
 
-from kale.embed.video_res3d import (
-    BasicBlock,
-    BasicFLowStem,
-    BasicStem,
-    Conv2Plus1D,
-    Conv3DNoTemporal,
-    Conv3DSimple,
-    R2Plus1dFlowStem,
-    R2Plus1dStem,
-    VideoResNet,
-)
+"""Add SELayers to MC3_18, R3D_18, R2plus1D_18"""
+
+from kale.embed.video_res3d import (BasicBlock, BasicFLowStem, BasicStem, Conv2Plus1D, Conv3DNoTemporal, Conv3DSimple,
+                                    R2Plus1dFlowStem, R2Plus1dStem, VideoResNet)
 from kale.embed.video_se_cnn import SELayerC, SELayerCoC, SELayerMAC, SELayerMC, SELayerT
+from torchvision.models.utils import load_state_dict_from_url
 
 model_urls = {
     "r3d_18": "https://download.pytorch.org/models/r3d_18-b3b3357e.pth",
@@ -20,10 +17,23 @@ model_urls = {
 }
 
 
-def _se_video_resnet(arch, attention, pretrained=False, progress=True, **kwargs):
-    model = VideoResNet(**kwargs)
+def _se_video_resnet_rgb(arch, attention, pretrained=False, progress=True, **kwargs):
+    """Add the several SELayers to MC3_18, R3D_18, R2plus1D_18 for RGB input.
 
-    n = 16
+    Args:
+        arch (string): the name of basic architecture. (Options: ["r3d_18", "mc3_18" and "r2plus1d_18"])
+        attention (string): the name of the SELayer.
+                        (Options: ["SELayerC", "SELayerT", "SELayerCoC", "SELayerMC", "SELayerMAC" and "SELayerCT"])
+        pretrained (bool): choose if pretrained parameters are used. (Default: False)
+        progress (bool, optional): whether or not to display a progress bar to stderr. (Default: True)
+
+    Returns:
+        model (VideoResNet): 3D convolution-based model with SELayers.
+    """
+    model = VideoResNet(**kwargs)
+    temporal_length = 16
+
+    # Add channel-wise SELayer
     if attention == "SELayerC":
         model.layer1._modules["0"].add_module("SELayerC", SELayerC(64))
         model.layer1._modules["1"].add_module("SELayerC", SELayerC(64))
@@ -34,16 +44,18 @@ def _se_video_resnet(arch, attention, pretrained=False, progress=True, **kwargs)
         model.layer4._modules["0"].add_module("SELayerC", SELayerC(512))
         model.layer4._modules["1"].add_module("SELayerC", SELayerC(512))
 
+    # Add temporal-wise SELayer
     elif attention == "SELayerT":
-        model.layer1._modules["0"].add_module("SELayerT", SELayerT(n))
-        model.layer1._modules["1"].add_module("SELayerT", SELayerT(n))
-        model.layer2._modules["0"].add_module("SELayerT", SELayerT(n // 2))
-        model.layer2._modules["1"].add_module("SELayerT", SELayerT(n // 2))
-        model.layer3._modules["0"].add_module("SELayerT", SELayerT(n // 4))
-        model.layer3._modules["1"].add_module("SELayerT", SELayerT(n // 4))
-        model.layer4._modules["0"].add_module("SELayerT", SELayerT(n // 8))
-        model.layer4._modules["1"].add_module("SELayerT", SELayerT(n // 8))
+        model.layer1._modules["0"].add_module("SELayerT", SELayerT(temporal_length))
+        model.layer1._modules["1"].add_module("SELayerT", SELayerT(temporal_length))
+        model.layer2._modules["0"].add_module("SELayerT", SELayerT(temporal_length // 2))
+        model.layer2._modules["1"].add_module("SELayerT", SELayerT(temporal_length // 2))
+        model.layer3._modules["0"].add_module("SELayerT", SELayerT(temporal_length // 4))
+        model.layer3._modules["1"].add_module("SELayerT", SELayerT(temporal_length // 4))
+        model.layer4._modules["0"].add_module("SELayerT", SELayerT(temporal_length // 8))
+        model.layer4._modules["1"].add_module("SELayerT", SELayerT(temporal_length // 8))
 
+    # Add convolution-based channel-wise SELayer
     elif attention == "SELayerCoC":
         model.layer1._modules["0"].add_module("SELayerCoC", SELayerCoC(64))
         model.layer1._modules["1"].add_module("SELayerCoC", SELayerCoC(64))
@@ -54,6 +66,7 @@ def _se_video_resnet(arch, attention, pretrained=False, progress=True, **kwargs)
         model.layer4._modules["0"].add_module("SELayerCoC", SELayerCoC(512))
         model.layer4._modules["1"].add_module("SELayerCoC", SELayerCoC(512))
 
+    # Add multi-pooling-based channel-wise SELayer
     elif attention == "SELayerMAC":
         model.layer1._modules["0"].add_module("SELayerMAC", SELayerMAC(64))
         model.layer1._modules["1"].add_module("SELayerMAC", SELayerMAC(64))
@@ -64,6 +77,7 @@ def _se_video_resnet(arch, attention, pretrained=False, progress=True, **kwargs)
         model.layer4._modules["0"].add_module("SELayerMAC", SELayerMAC(512))
         model.layer4._modules["1"].add_module("SELayerMAC", SELayerMAC(512))
 
+    # Add max-pooling-based channel-wise SELayer
     elif attention == "SELayerMC":
         model.layer1._modules["0"].add_module("SELayerMC", SELayerMC(64))
         model.layer1._modules["1"].add_module("SELayerMC", SELayerMC(64))
@@ -74,6 +88,7 @@ def _se_video_resnet(arch, attention, pretrained=False, progress=True, **kwargs)
         model.layer4._modules["0"].add_module("SELayerMC", SELayerMC(512))
         model.layer4._modules["1"].add_module("SELayerMC", SELayerMC(512))
 
+    # Add channel-temporal-wise SELayer
     elif attention == "SELayerCT":
         model.layer1._modules["0"].add_module("SELayerC", SELayerC(64))
         model.layer1._modules["1"].add_module("SELayerC", SELayerC(64))
@@ -84,12 +99,12 @@ def _se_video_resnet(arch, attention, pretrained=False, progress=True, **kwargs)
         model.layer4._modules["0"].add_module("SELayerC", SELayerC(512))
         model.layer4._modules["1"].add_module("SELayerC", SELayerC(512))
 
-        model.layer1._modules["0"].add_module("SELayerT", SELayerT(n))
-        model.layer1._modules["1"].add_module("SELayerT", SELayerT(n))
-        model.layer2._modules["0"].add_module("SELayerT", SELayerT(n // 2))
-        model.layer2._modules["1"].add_module("SELayerT", SELayerT(n // 2))
-        model.layer3._modules["0"].add_module("SELayerT", SELayerT(n // 4))
-        model.layer3._modules["1"].add_module("SELayerT", SELayerT(n // 4))
+        model.layer1._modules["0"].add_module("SELayerT", SELayerT(temporal_length))
+        model.layer1._modules["1"].add_module("SELayerT", SELayerT(temporal_length))
+        model.layer2._modules["0"].add_module("SELayerT", SELayerT(temporal_length // 2))
+        model.layer2._modules["1"].add_module("SELayerT", SELayerT(temporal_length // 2))
+        model.layer3._modules["0"].add_module("SELayerT", SELayerT(temporal_length // 4))
+        model.layer3._modules["1"].add_module("SELayerT", SELayerT(temporal_length // 4))
 
     if pretrained:
         state_dict = load_state_dict_from_url(model_urls[arch], progress=progress)
@@ -98,9 +113,11 @@ def _se_video_resnet(arch, attention, pretrained=False, progress=True, **kwargs)
 
 
 def _se_video_resnet_flow(arch, attention, pretrained=False, progress=True, **kwargs):
+    """Add the several SELayers to MC3_18, R3D_18, R2plus1D_18 for optical flow input."""
     model = VideoResNet(**kwargs)
+    temporal_length = 16
 
-    n = 16
+    # Add channel-wise SELayer
     if attention == "SELayerC":
         model.layer1._modules["0"].add_module("SELayerC", SELayerC(64))
         model.layer1._modules["1"].add_module("SELayerC", SELayerC(64))
@@ -111,14 +128,16 @@ def _se_video_resnet_flow(arch, attention, pretrained=False, progress=True, **kw
         model.layer4._modules["0"].add_module("SELayerC", SELayerC(512))
         model.layer4._modules["1"].add_module("SELayerC", SELayerC(512))
 
+    # Add temporal-wise SELayer
     elif attention == "SELayerT":
-        model.layer1._modules["0"].add_module("SELayerT", SELayerT(n // 2))
-        model.layer1._modules["1"].add_module("SELayerT", SELayerT(n // 2))
-        model.layer2._modules["0"].add_module("SELayerT", SELayerT(n // 4))
-        model.layer2._modules["1"].add_module("SELayerT", SELayerT(n // 4))
-        model.layer3._modules["0"].add_module("SELayerT", SELayerT(n // 8))
-        model.layer3._modules["1"].add_module("SELayerT", SELayerT(n // 8))
+        model.layer1._modules["0"].add_module("SELayerT", SELayerT(temporal_length // 2))
+        model.layer1._modules["1"].add_module("SELayerT", SELayerT(temporal_length // 2))
+        model.layer2._modules["0"].add_module("SELayerT", SELayerT(temporal_length // 4))
+        model.layer2._modules["1"].add_module("SELayerT", SELayerT(temporal_length // 4))
+        model.layer3._modules["0"].add_module("SELayerT", SELayerT(temporal_length // 8))
+        model.layer3._modules["1"].add_module("SELayerT", SELayerT(temporal_length // 8))
 
+    # Add convolution-based channel-wise SELayer
     elif attention == "SELayerCoC":
         model.layer1._modules["0"].add_module("SELayerCoC", SELayerCoC(64))
         model.layer1._modules["1"].add_module("SELayerCoC", SELayerCoC(64))
@@ -129,6 +148,7 @@ def _se_video_resnet_flow(arch, attention, pretrained=False, progress=True, **kw
         model.layer4._modules["0"].add_module("SELayerCoC", SELayerCoC(512))
         model.layer4._modules["1"].add_module("SELayerCoC", SELayerCoC(512))
 
+    # Add multi-pooling-based channel-wise SELayer
     elif attention == "SELayerMAC":
         model.layer1._modules["0"].add_module("SELayerMAC", SELayerMAC(64))
         model.layer1._modules["1"].add_module("SELayerMAC", SELayerMAC(64))
@@ -139,6 +159,7 @@ def _se_video_resnet_flow(arch, attention, pretrained=False, progress=True, **kw
         model.layer4._modules["0"].add_module("SELayerMAC", SELayerMAC(512))
         model.layer4._modules["1"].add_module("SELayerMAC", SELayerMAC(512))
 
+    # Add max-pooling-based channel-wise SELayer
     elif attention == "SELayerMC":
         model.layer1._modules["0"].add_module("SELayerMC", SELayerMC(64))
         model.layer1._modules["1"].add_module("SELayerMC", SELayerMC(64))
@@ -149,6 +170,24 @@ def _se_video_resnet_flow(arch, attention, pretrained=False, progress=True, **kw
         model.layer4._modules["0"].add_module("SELayerMC", SELayerMC(512))
         model.layer4._modules["1"].add_module("SELayerMC", SELayerMC(512))
 
+    # Add channel-temporal-wise SELayer
+    elif attention == "SELayerCT":
+        model.layer1._modules["0"].add_module("SELayerC", SELayerC(64))
+        model.layer1._modules["1"].add_module("SELayerC", SELayerC(64))
+        model.layer2._modules["0"].add_module("SELayerC", SELayerC(128))
+        model.layer2._modules["1"].add_module("SELayerC", SELayerC(128))
+        model.layer3._modules["0"].add_module("SELayerC", SELayerC(256))
+        model.layer3._modules["1"].add_module("SELayerC", SELayerC(256))
+        model.layer4._modules["0"].add_module("SELayerC", SELayerC(512))
+        model.layer4._modules["1"].add_module("SELayerC", SELayerC(512))
+
+        model.layer1._modules["0"].add_module("SELayerT", SELayerT(temporal_length // 2))
+        model.layer1._modules["1"].add_module("SELayerT", SELayerT(temporal_length // 2))
+        model.layer2._modules["0"].add_module("SELayerT", SELayerT(temporal_length // 4))
+        model.layer2._modules["1"].add_module("SELayerT", SELayerT(temporal_length // 4))
+        model.layer3._modules["0"].add_module("SELayerT", SELayerT(temporal_length // 8))
+        model.layer3._modules["1"].add_module("SELayerT", SELayerT(temporal_length // 8))
+
     if pretrained:
         state_dict = load_state_dict_from_url(model_urls[arch], progress=progress)
         state_dict.pop("stem.0.weight")
@@ -157,7 +196,7 @@ def _se_video_resnet_flow(arch, attention, pretrained=False, progress=True, **kw
 
 
 def se_r3d_18_rgb(attention, pretrained=False, progress=True, **kwargs):
-    return _se_video_resnet(
+    return _se_video_resnet_rgb(
         "r3d_18",
         attention,
         pretrained,
@@ -185,7 +224,7 @@ def se_r3d_18_flow(attention, pretrained=False, progress=True, **kwargs):
 
 
 def se_mc3_18_rgb(attention, pretrained=False, progress=True, **kwargs):
-    return _se_video_resnet(
+    return _se_video_resnet_rgb(
         "mc3_18",
         attention,
         pretrained,
@@ -199,7 +238,7 @@ def se_mc3_18_rgb(attention, pretrained=False, progress=True, **kwargs):
 
 
 def se_mc3_18_flow(attention, pretrained=False, progress=True, **kwargs):
-    return _se_video_resnet(
+    return _se_video_resnet_flow(
         "mc3_18",
         attention,
         pretrained,
@@ -213,7 +252,7 @@ def se_mc3_18_flow(attention, pretrained=False, progress=True, **kwargs):
 
 
 def se_r2plus1d_18_rgb(attention, pretrained=False, progress=True, **kwargs):
-    return _se_video_resnet(
+    return _se_video_resnet_rgb(
         "r2plus1d_18",
         attention,
         pretrained,
@@ -227,7 +266,7 @@ def se_r2plus1d_18_rgb(attention, pretrained=False, progress=True, **kwargs):
 
 
 def se_r2plus1d_18_flow(attention, pretrained=False, progress=True, **kwargs):
-    return _se_video_resnet(
+    return _se_video_resnet_flow(
         "r2plus1d_18",
         attention,
         pretrained,
@@ -241,42 +280,45 @@ def se_r2plus1d_18_flow(attention, pretrained=False, progress=True, **kwargs):
 
 
 def se_r3d(attention, rgb=False, flow=False, pretrained=False, progress=True):
-    """Get R3D_18 models."""
+    """Get R3D_18 models with SELayers for different inputs.
+
+    Args:
+        attention (string): the name of the SELayer.
+        rgb (bool): choose if RGB model is needed. (Default: False)
+        flow (bool): choose if optical flow model is needed. (Default: False)
+        pretrained (bool): choose if pretrained parameters are used. (Default: False)
+        progress (bool, optional): whether or not to display a progress bar to stderr. (Default: True)
+
+    Returns:
+        models (dictionary): A dictionary contains models for RGB and optical flow.
+    """
+
     r3d_rgb = r3d_flow = None
-    if rgb and not flow:
+    if rgb:
         r3d_rgb = se_r3d_18_rgb(attention=attention, pretrained=pretrained, progress=progress)
-    elif not rgb and flow:
-        r3d_flow = se_r3d_18_flow(attention=attention, pretrained=pretrained, progress=progress)
-    elif rgb and flow:
-        r3d_rgb = se_r3d_18_rgb(attention=attention, pretrained=pretrained, progress=progress)
+    if flow:
         r3d_flow = se_r3d_18_flow(attention=attention, pretrained=pretrained, progress=progress)
     models = {"rgb": r3d_rgb, "flow": r3d_flow}
     return models
 
 
 def se_mc3(attention, rgb=False, flow=False, pretrained=False, progress=True):
-    """Get MC3_18 models."""
+    """Get MC3_18 models with SELayers for different inputs."""
     mc3_rgb = mc3_flow = None
-    if rgb and not flow:
+    if rgb:
         mc3_rgb = se_mc3_18_rgb(attention=attention, pretrained=pretrained, progress=progress)
-    elif not rgb and flow:
-        mc3_flow = se_mc3_18_flow(attention=attention, pretrained=pretrained, progress=progress)
-    elif rgb and flow:
-        mc3_rgb = se_mc3_18_rgb(attention=attention, pretrained=pretrained, progress=progress)
+    if flow:
         mc3_flow = se_mc3_18_flow(attention=attention, pretrained=pretrained, progress=progress)
     models = {"rgb": mc3_rgb, "flow": mc3_flow}
     return models
 
 
 def se_r2plus1d(attention, rgb=False, flow=False, pretrained=False, progress=True):
-    """Get R2+1D_18 models."""
+    """Get R2+1D_18 models with SELayers for different inputs."""
     r2plus1d_rgb = r2plus1d_flow = None
-    if rgb and not flow:
+    if rgb:
         r2plus1d_rgb = se_r2plus1d_18_rgb(attention=attention, pretrained=pretrained, progress=progress)
-    elif not rgb and flow:
-        r2plus1d_flow = se_r2plus1d_18_flow(attention=attention, pretrained=pretrained, progress=progress)
-    elif rgb and flow:
-        r2plus1d_rgb = se_r2plus1d_18_rgb(attention=attention, pretrained=pretrained, progress=progress)
+    if flow:
         r2plus1d_flow = se_r2plus1d_18_flow(attention=attention, pretrained=pretrained, progress=progress)
     models = {"rgb": r2plus1d_rgb, "flow": r2plus1d_flow}
     return models
