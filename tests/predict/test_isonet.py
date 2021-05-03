8129610d12ae0a3eedcaf4011478fab996a5345c
Haiping Lu <haipinglu@users.noreply.github.com>
2021-05-04 00:05:58 +0100

    cover isonet 75%

--- tests/predict/test_isonet.py
+++ tests/predict/test_isonet.py
@@ -3,6 +3,8 @@ import torch
 
 import kale.predict.isonet as isonet
 
+BATCH_SIZE = 2
+INPUT_BATCH = torch.randn(BATCH_SIZE, 3, 224, 224)
 TRANS_FUNS = ["basic_transform"]  # "bottleneck_transform"]
 
 
@@ -11,14 +13,14 @@ def testing_cfg(download_path):
     config_params = {
         "net_params": {
             "use_dirac": True,
-            "use_dropout": False,
+            "use_dropout": True,
             "dropout_rate": 0.0,
             "nc": 10,
             "depths": 34,
-            "has_bn": False,
+            "has_bn": True,
             "use_srelu": True,
             "transfun": "basic_transform",
-            "has_st": False,
+            "has_st": True,
         }
     }
     yield config_params
@@ -26,9 +28,9 @@ def testing_cfg(download_path):
 
 @pytest.mark.parametrize("transfun", TRANS_FUNS)
 def test_isonet(transfun, testing_cfg):
-
     net_params = testing_cfg["net_params"]
     net_params["transfun"] = transfun
     net = isonet.ISONet(net_params)
-
-    assert isinstance(net, torch.nn.Module)
+    net.eval()
+    output_batch = net(INPUT_BATCH)
+    assert output_batch.size() == (BATCH_SIZE, net_params["nc"])
