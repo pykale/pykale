b5f32e9c0736f4e9f2a7b871b0ff5a26cf5cfa0b
Haiping Lu <haipinglu@users.noreply.github.com>
2021-05-03 11:42:26 +0100

    fix csv logging and add append tests

--- tests/utils/test_csv_logger.py
+++ tests/utils/test_csv_logger.py
@@ -6,7 +6,7 @@ from kale.utils.csv_logger import setup_logger
 
 
 def test_csv_logger(download_path):
-    train_params = {"para1": 1, "para2": 2}
+    train_params = {"param1": 1, "param2": 2}
     method = "DANN"
     seed = 32  # To define in conftest later?
     testing_logger, results, _, test_csv_file = setup_logger(train_params, download_path, method, seed)
@@ -18,9 +18,15 @@ def test_csv_logger(download_path):
     #         is_validation=False, method_name=method, seed=seed, metric_values=df_metrics,
     #     )
     # results.print_score(method)
+    results.append_to_txt(os.path.join(download_path, "temp.txt"), train_params, 3)
+    results.append_to_markdown(os.path.join(download_path, "temp.md"), train_params, 3)
     results.to_csv(test_csv_file)
     assert os.path.isfile(test_csv_file)
+    assert os.path.isfile(os.path.join(download_path, "temp.txt"))
+    assert os.path.isfile(os.path.join(download_path, "temp.md"))
 
     # Teardown log file
     os.remove(test_csv_file)
     os.remove(os.path.join(download_path, "parameters.json"))
+    os.remove(os.path.join(download_path, "temp.txt"))
+    os.remove(os.path.join(download_path, "temp.md"))
