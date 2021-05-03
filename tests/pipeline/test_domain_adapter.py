6f1f28293cce0634f91582142cb04e09eed0c02d
Haiping Lu <haipinglu@users.noreply.github.com>
2021-05-03 09:17:15 +0100

    add trainer.test

--- tests/pipeline/test_domain_adapter.py
+++ tests/pipeline/test_domain_adapter.py
@@ -100,5 +100,6 @@ def test_domain_adaptor(DA_method, n_fewshot, download_path, testing_cfg):
 
     trainer = pl.Trainer(min_epochs=train_params["nb_init_epochs"], max_epochs=train_params["nb_adapt_epochs"], gpus=0)
     trainer.fit(model)
+    trainer.test()
     metric_values = trainer.callback_metrics
     assert isinstance(metric_values, dict)
