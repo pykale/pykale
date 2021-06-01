aa1ce111eee753e60c7417918a8cd1edce44fc2d
Xianyuan <xianyuan.liu@sheffield.ac.uk>
2021-06-01 02:16:37 +0100

    Change drop_last to False

--- kale/loaddata/sampler.py
+++ kale/loaddata/sampler.py
@@ -61,7 +61,8 @@ class FixedSeedSamplingConfig(SamplingConfig):
                 )
             else:
                 sub_sampler = RandomSampler(dataset, generator=torch.Generator().manual_seed(self._seed))
-            sampler = BatchSampler(sub_sampler, batch_size=batch_size, drop_last=True)
+            # sampler = BatchSampler(sub_sampler, batch_size=batch_size, drop_last=True)
+            sampler = BatchSampler(sub_sampler, batch_size=batch_size, drop_last=False)
         return torch.utils.data.DataLoader(dataset=dataset, batch_sampler=sampler)
 
 
