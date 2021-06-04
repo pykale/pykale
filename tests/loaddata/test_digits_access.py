1ecca7d018f471521c3484dde106dfee7fb82fe7
Mustafa Chasmai <mustchasmai@gmail.com>
2021-06-04 18:43:25 +0530

    tests added for class-subsampling

--- tests/loaddata/test_digits_access.py
+++ tests/loaddata/test_digits_access.py
@@ -15,6 +15,8 @@ WEIGHT_TYPE = ["natural", "balanced", "preset0"]
 DATASIZE_TYPE = ["max", "source"]
 VAL_RATIO = [0.1]
 
+CLASS_SUB_SAMPLES = [list(range(i, j + 1)) for i in range(0, 8) for j in range(6, 10) if i <= j and j < i + 3]
+
 
 @pytest.mark.parametrize("source_name", SOURCES)
 @pytest.mark.parametrize("target_name", TARGETS)
@@ -51,3 +53,29 @@ def test_get_train_test(dataset_name, download_path):
     assert source.n_classes() == 10
     assert isinstance(source_train, torch.utils.data.Dataset)
     assert isinstance(source_test, torch.utils.data.Dataset)
+
+
+@pytest.mark.parametrize("dataset_name", SOURCES + TARGETS)
+@pytest.mark.parametrize("weight_type", WEIGHT_TYPE)
+@pytest.mark.parametrize("datasize_type", DATASIZE_TYPE)
+@pytest.mark.parametrize("val_ratio", VAL_RATIO)
+@pytest.mark.parametrize("class_sub_sample", CLASS_SUB_SAMPLES)
+def test_class_subsampling(dataset_name, weight_type, datasize_type, download_path, val_ratio, class_sub_sample):
+    source, target, num_channels = DigitDataset.get_source_target(
+        DigitDataset(dataset_name), DigitDataset(dataset_name), download_path
+    )
+
+    source_train = source.get_class_subsampled_train(class_sub_sample)
+    source_test = source.get_class_subsampled_test(class_sub_sample)
+    source_train_val = source.get_train_val(val_ratio, class_sub_sample)
+
+    dataset_subsampled = MultiDomainDatasets(
+        source, target, config_weight_type=weight_type, config_size_type=datasize_type, sub_class_ids=class_sub_sample
+    )
+
+    assert isinstance(source_train, torch.utils.data.Dataset)
+    assert isinstance(source_test, torch.utils.data.Dataset)
+    assert isinstance(source_train_val, list)
+    assert isinstance(source_train_val[0], torch.utils.data.Dataset)
+    assert isinstance(source_train_val[1], torch.utils.data.Dataset)
+    assert isinstance(dataset_subsampled, DomainsDatasetBase)
