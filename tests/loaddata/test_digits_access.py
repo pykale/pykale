2e27def0b0717e2b147648d1213db34682278a59
Haiping Lu <haipinglu@users.noreply.github.com>
2021-06-14 17:13:56 +0100

    remove MNIST

--- tests/loaddata/test_digits_access.py
+++ tests/loaddata/test_digits_access.py
@@ -10,7 +10,7 @@ from kale.loaddata.multi_domain import DomainsDatasetBase, MultiDomainDatasets
 
 SOURCES = ["MNIST", "USPS"]
 TARGETS = ["MNISTM", "SVHN"]
-ALL = SOURCES + TARGETS  # ["SVHN", "USPS"]
+ALL = ["SVHN", "USPS"]  # SOURCES + TARGETS
 
 WEIGHT_TYPE = ["natural", "balanced", "preset0"]
 DATASIZE_TYPE = ["max", "source"]
