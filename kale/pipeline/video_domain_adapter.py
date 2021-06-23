3c87e0be17d087d14d7be0c85908500b755230dc
Xianyuan <xianyuan.liu@sheffield.ac.uk>
2021-06-23 16:18:24 +0100

    Change loss from 1000* back to 1*

--- kale/pipeline/video_domain_adapter.py
+++ kale/pipeline/video_domain_adapter.py
@@ -147,7 +147,7 @@ class BaseAdaptTrainerVideo(BaseAdaptTrainer):
             loss = task_loss
         else:
             # loss = task_loss
-            loss = task_loss + self.lamb_da * adv_loss * 1000
+            loss = task_loss + self.lamb_da * adv_loss
 
         log_metrics = get_aggregated_metrics_from_dict(log_metrics)
         log_metrics.update(get_metrics_from_parameter_dict(self.get_parameters_watch_list(), loss.device))
