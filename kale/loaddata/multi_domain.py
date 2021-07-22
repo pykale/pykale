163aa93fea776f677481093e711472a1384c751d
Shuo Zhou <szhou20@sheffield.ac.uk>
2021-07-22 21:55:48 +0100

    init multidomain image reader

--- kale/loaddata/multi_domain.py
+++ kale/loaddata/multi_domain.py
@@ -3,12 +3,15 @@ Construct a dataset with (multiple) source and target domains, from https://gith
 """
 
 import logging
+import os
 from enum import Enum
-from typing import Dict
+from typing import Any, Callable, cast, Dict, List, Optional, Tuple
 
 import numpy as np
 import torch.utils.data
 from sklearn.utils import check_random_state
+from torchvision.datasets import VisionDataset
+from torchvision.datasets.folder import default_loader, has_file_allowed_extension, IMG_EXTENSIONS
 
 from kale.loaddata.dataset_access import DatasetAccess, get_class_subset
 from kale.loaddata.sampler import get_labels, MultiDataLoader, SamplingConfig
@@ -229,3 +232,174 @@ def _split_dataset_few_shot(dataset, n_fewshot, random_state=None):
     labeled_dataset = torch.utils.data.Subset(dataset, tindices)
     unlabeled_dataset = torch.utils.data.Subset(dataset, uindices)
     return labeled_dataset, unlabeled_dataset
+
+
+class MultiDomainImageFolder(VisionDataset):
+    """A generic data loader where the samples are arranged in this way: ::
+            root/domain_a/class_x/xxx.ext
+            root/domain_a/class_x/xxy.ext
+            ...
+            root/domain_a/class_y/xxz.ext
+            ...
+            root/domain_k/class_x/123.ext
+            root/domain_k/class_x/abc3.ext
+            ...
+            root/domain_k/class_y/asd932_.ext
+        Args:
+            root (string): Root directory path.
+            loader (callable): A function to load a sample given its path.
+            extensions (tuple[string]): A list of allowed extensions.
+                both extensions and is_valid_file should not be passed.
+            transform (callable, optional): A function/transform that takes in
+                a sample and returns a transformed version.
+                E.g, ``transforms.RandomCrop`` for images.
+            target_transform (callable, optional): A function/transform that takes
+                in the target and transforms it.
+            is_valid_file (callable, optional): A function that takes path of a file
+                and check if the file is a valid file (used to check of corrupt files)
+                both extensions and is_valid_file should not be passed.
+         Attributes:
+            classes (list): List of the class names sorted alphabetically.
+            class_to_idx (dict): Dict with items (class_name, class_index).
+            samples (list): List of (sample path, class_index) tuples
+            targets (list): The class_index value for each image in the dataset
+            domains (list): List of the domain names sorted alphabetically.
+            domain_to_idx (dict): Dict with items (domain_name, domain_index).
+            domain_labels (list): The domain_index value for each image in the dataset
+        """
+
+    def __init__(
+        self,
+        root: str,
+        loader: Callable[[str], Any] = default_loader,
+        extensions: Optional[Tuple[str, ...]] = IMG_EXTENSIONS,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+        domains: Optional[List] = None,
+        is_valid_file: Optional[Callable[[str], bool]] = None,
+        return_domain_label: Optional[bool] = False,
+    ) -> None:
+        super(MultiDomainImageFolder, self).__init__(root, transform=transform, target_transform=target_transform)
+        domains_, domain_to_idx = self._find_classes(self.root)
+        if type(domains) == list:
+            for domain_name in domains:
+                if domain_name not in domains_:
+                    raise ValueError("Domain %s not in the image directory" % domain_name)
+            domain_to_idx = {domain_name: i for i, domain_name in enumerate(domains)}
+        elif domains is None:
+            domains = domains_
+        else:
+            raise ValueError("Unsupported type for variable domains")
+        classes, class_to_idx = self._find_classes(os.path.join(self.root, domains[0]))
+        for domain in domains:
+            domain_path = os.path.join(self.root, domain)
+            classes_, class_to_idx_ = self._find_classes(domain_path)
+            if not classes == classes_:
+                raise ValueError("Classes for different domains are expected to be the same.")
+        samples = make_multi_domain_set(self.root, class_to_idx, domain_to_idx, extensions, is_valid_file)
+        if len(samples) == 0:
+            msg = "Found 0 files in sub-folders of: {}\n".format(self.root)
+            if extensions is not None:
+                msg += "Supported extensions are: {}".format(",".join(extensions))
+            raise RuntimeError(msg)
+
+        self.loader = loader
+        self.extensions = extensions
+
+        self.classes = classes
+        self.class_to_idx = class_to_idx
+        self.samples = samples
+        self.targets = [s[1] for s in samples]
+        self.domains = domains
+        self.domain_to_idx = domain_to_idx
+        self.domain_labels = [s[2] for s in samples]
+        self.return_domain_label = return_domain_label
+
+    @staticmethod
+    def _find_classes(directory: str) -> Tuple[List[str], Dict[str, int]]:
+        """
+            Finds the class folders in a dataset.
+            Args:
+                directory (string): Directory path.
+            Returns:
+                tuple: (classes, class_to_idx) where classes are relative to (dir), and class_to_idx is a dictionary.
+            Ensures:
+                No class is a subdirectory of another.
+            """
+        classes = [d.name for d in os.scandir(directory) if d.is_dir()]
+        classes.sort()
+        class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
+        return classes, class_to_idx
+
+    def __getitem__(self, index: int) -> Tuple:
+        """
+            Args:
+                index (int): Index
+            Returns:
+                tuple: (sample, target, domain) where target is class_index of the target class.
+            """
+        path, target, domain = self.samples[index]
+        sample = self.loader(path)
+        if self.transform is not None:
+            sample = self.transform(sample)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        if self.return_domain_label:
+            return sample, target, domain
+        else:
+            return sample, target
+
+    def __len__(self) -> int:
+        return len(self.samples)
+
+
+def make_multi_domain_set(
+    directory: str,
+    class_to_idx: Dict[str, int],
+    domain_to_idx: Dict[str, int],
+    extensions: Optional[Tuple[str, ...]] = None,
+    is_valid_file: Optional[Callable[[str], bool]] = None,
+) -> List[Tuple[str, int, int]]:
+    """Generates a list of samples of a form (path_to_sample, class, domain).
+    Args:
+        directory (str): root dataset directory
+        class_to_idx (Dict[str, int]): dictionary mapping class name to class index
+        domain_to_idx (Dict[str, int]): dictionary mapping d name to class index
+        extensions (optional): A list of allowed extensions.
+            Either extensions or is_valid_file should be passed. Defaults to None.
+        is_valid_file (optional): A function that takes path of a file
+            and checks if the file is a valid file
+            (used to check of corrupt files) both extensions and
+            is_valid_file should not be passed. Defaults to None.
+    Raises:
+        ValueError: In case ``extensions`` and ``is_valid_file`` are None or both are not None.
+    Returns:
+        List[Tuple[str, int, int]]: samples of a form (path_to_sample, class, domain)
+    """
+    instances = []
+    directory = os.path.expanduser(directory)
+    both_none = extensions is None and is_valid_file is None
+    both_something = extensions is not None and is_valid_file is not None
+    if both_none or both_something:
+        raise ValueError("Both extensions and is_valid_file cannot be None or not None at the same time")
+    if extensions is not None:
+
+        def is_valid_file(x: str) -> bool:
+            return has_file_allowed_extension(x, cast(Tuple[str, ...], extensions))
+
+    is_valid_file = cast(Callable[[str], bool], is_valid_file)
+    for target_domain in sorted(domain_to_idx.keys()):
+        domain_index = domain_to_idx[target_domain]
+        domain_dir = os.path.join(directory, target_domain)
+        for target_class in sorted(class_to_idx.keys()):
+            class_index = class_to_idx[target_class]
+            target_dir = os.path.join(domain_dir, target_class)
+            if not os.path.isdir(target_dir):
+                continue
+            for root, _, fnames in sorted(os.walk(target_dir, followlinks=True)):
+                for fname in sorted(fnames):
+                    path = os.path.join(root, fname)
+                    if is_valid_file(path):
+                        item = path, class_index, domain_index
+                        instances.append(item)
+    return instances
