ae659e4f85690129bcab5f73f96bbe1069d54d5d
Shuo Zhou <szhou20@sheffield.ac.uk>
2021-08-11 02:37:19 +0100

    fix linting cmr main and notebook

--- examples/cmri_mpca/main.py
+++ examples/cmri_mpca/main.py
@@ -1,15 +1,23 @@
+"""
+PyKale Tutorial: A Machine Learning Pipeline for PAH Diagnosis
+
+Reference:
+Swift, A. J., Lu, H., Uthoff, J., Garg, P., Cogliano, M., Taylor, J., ... & Kiely, D. G. (2020). A machine learning
+cardiac magnetic resonance approach to extract disease features and automate pulmonary arterial hypertension diagnosis.
+European Heart Journal-Cardiovascular Imaging. https://academic.oup.com/ehjcimaging/article/22/2/236/5717931
+"""
 import os
 
-from config import get_cfg_defaults, read_dicom_imgs
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from config import get_cfg_defaults, read_dicom_imgs
 from sklearn.model_selection import cross_validate
 
-from kale.utils.download import download_file_by_url
-from kale.prepdata.image_transform import mask_img_stack, reg_img_stack, rescale_img_stack, normalize_img_stack
 from kale.interpret import model_weights
 from kale.pipeline.mpca_trainer import MPCATrainer
+from kale.prepdata.image_transform import mask_img_stack, normalize_img_stack, reg_img_stack, rescale_img_stack
+from kale.utils.download import download_file_by_url
 
 
 def visualize_imgs(imgs, landmarks=None):
@@ -20,7 +28,7 @@ def visualize_imgs(imgs, landmarks=None):
 
     for i in range(imgs.shape[0]):
         fig.add_subplot(rows, columns, i + 1)
-        plt.axis('off')
+        plt.axis("off")
         plt.imshow(imgs[i, 0, ...])
         if landmarks is not None:
             coords = landmarks.iloc[i, :].values.reshape((-1, 2))
@@ -28,8 +36,15 @@ def visualize_imgs(imgs, landmarks=None):
             for j in range(n_landmark):
                 ix = coords[j, 0]
                 iy = coords[j, 1]
-                plt.plot(ix, iy, marker='o', markersize=5, markerfacecolor=(1, 1, 1, 0.1), markeredgewidth=1.5,
-                         markeredgecolor='r')
+                plt.plot(
+                    ix,
+                    iy,
+                    marker="o",
+                    markersize=5,
+                    markerfacecolor=(1, 1, 1, 0.1),
+                    markeredgewidth=1.5,
+                    markeredgecolor="r",
+                )
         plt.title(i + 1)
 
     plt.show()
@@ -55,9 +70,9 @@ def main():
     mask = read_dicom_imgs(mask_path)
 
     df_file = os.path.join(cfg.DATASET.ROOT, base_dir, cfg.DATASET.LANDMARK_FILE)
-    df = pd.read_csv(df_file, index_col='Subject')
+    df = pd.read_csv(df_file, index_col="Subject")
     landmarks = df.iloc[:, :6]
-    y = df['Group'].values
+    y = df["Group"].values
     y[np.where(y != 0)] = 1
 
     visualize_imgs(imgs, landmarks=landmarks)
@@ -79,10 +94,10 @@ def main():
     # ---- evaluating machine learning pipeline ----
     x = img_norm.copy()
     trainer = MPCATrainer(n_features=200)
-    cv_results = cross_validate(trainer, x, y, cv=10, scoring=['accuracy', 'roc_auc'], n_jobs=1)
+    cv_results = cross_validate(trainer, x, y, cv=10, scoring=["accuracy", "roc_auc"], n_jobs=1)
 
-    print("Accuracy: ", np.mean(cv_results['test_accuracy']))
-    print("AUC: ", np.mean(cv_results['test_roc_auc']))
+    print("Accuracy: ", np.mean(cv_results["test_accuracy"]))
+    print("AUC: ", np.mean(cv_results["test_roc_auc"]))
 
     # ---- model weights interpretation ----
     trainer.fit(x, y)
