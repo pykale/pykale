7449f92af1e6af9af4f46b04477a51e19df09053
Jakamar <jakamar1999@yahoo.com>
2021-10-22 12:17:18 +0100

    Fix #233 formatted the code
    1)removed capital letters from the variables name within the program
    2)used us spelling

--- examples/cmri_mpca/main.py
+++ examples/cmri_mpca/main.py
@@ -39,15 +39,15 @@ def main():
     cfg.freeze()
     print(cfg)
 
-    SAVE_IMAGES = cfg.OUTPUT.SAVE_IMAGES
-    print(f"Save Images: {SAVE_IMAGES}")
+    save_images = cfg.OUTPUT.SAVE_IMAGES
+    print(f"Save Images: {save_images}")
 
-    # ---- initialise folder to store images ----
-    SAVE_IMAGES_LOCATION = cfg.OUTPUT.ROOT
-    print(f"Save Images: {SAVE_IMAGES_LOCATION}")
+    # ---- initialize folder to store images ----
+    save_images_location = cfg.OUTPUT.ROOT
+    print(f"Save Images: {save_images_location}")
 
-    if not os.path.exists(SAVE_IMAGES_LOCATION):
-        os.makedirs(SAVE_IMAGES_LOCATION)
+    if not os.path.exists(save_images_location):
+        os.makedirs(save_images_location)
 
     # ---- setup dataset ----
     base_dir = cfg.DATASET.BASE_DIR
@@ -67,41 +67,41 @@ def main():
     y[np.where(y != 0)] = 1  # convert to binary classification problem, i.e. no PH vs PAH
 
     # plot the first phase of images
-    if SAVE_IMAGES:
+    if save_images:
         visualize.plot_multi_images(
             images[:, 0, ...],
             marker_locs=landmarks,
             im_kwargs=dict(cfg.IM_KWARGS),
             marker_kwargs=dict(cfg.MARKER_KWARGS),
-        ).savefig(str(SAVE_IMAGES_LOCATION) + "/0)first_phase.png")
+        ).savefig(str(save_images_location) + "/0)first_phase.png")
 
     # ---- data pre-processing ----
     # ----- image registration -----
     img_reg, max_dist = reg_img_stack(images.copy(), landmarks)
-    if SAVE_IMAGES:
+    if save_images:
         visualize.plot_multi_images(img_reg[:, 0, ...], im_kwargs=dict(cfg.IM_KWARGS)).savefig(
-            str(SAVE_IMAGES_LOCATION) + "/1)image_registration"
+            str(save_images_location) + "/1)image_registration"
         )
 
     # ----- masking -----
     img_masked = mask_img_stack(img_reg.copy(), mask[0, 0, ...])
-    if SAVE_IMAGES:
+    if save_images:
         visualize.plot_multi_images(img_masked[:, 0, ...], im_kwargs=dict(cfg.IM_KWARGS)).savefig(
-            str(SAVE_IMAGES_LOCATION) + "/2)masking"
+            str(save_images_location) + "/2)masking"
         )
 
     # ----- resize -----
     img_rescaled = rescale_img_stack(img_masked.copy(), scale=1 / cfg.PROC.SCALE)
-    if SAVE_IMAGES:
+    if save_images:
         visualize.plot_multi_images(img_rescaled[:, 0, ...], im_kwargs=dict(cfg.IM_KWARGS)).savefig(
-            str(SAVE_IMAGES_LOCATION) + "/3)resize"
+            str(save_images_location) + "/3)resize"
         )
 
     # ----- normalization -----
     img_norm = normalize_img_stack(img_rescaled.copy())
-    if SAVE_IMAGES:
+    if save_images:
         visualize.plot_multi_images(img_norm[:, 0, ...], im_kwargs=dict(cfg.IM_KWARGS)).savefig(
-            str(SAVE_IMAGES_LOCATION) + "/4)normalize"
+            str(save_images_location) + "/4)normalize"
         )
 
     # ---- evaluating machine learning pipeline ----
@@ -121,13 +121,13 @@ def main():
     weights = rescale_img_stack(weights, cfg.PROC.SCALE)  # rescale weights to original shape
     weights = mask_img_stack(weights, mask[0, 0, ...])  # masking weights
     top_weights = model_weights.select_top_weight(weights, select_ratio=0.02)  # select top 2% weights
-    if SAVE_IMAGES:
+    if save_images:
         visualize.plot_weights(
             top_weights[0][0],
             background_img=images[0][0],
             im_kwargs=dict(cfg.IM_KWARGS),
             marker_kwargs=dict(cfg.WEIGHT_KWARGS),
-        ).savefig(str(SAVE_IMAGES_LOCATION) + "/5)weights")
+        ).savefig(str(save_images_location) + "/5)weights")
 
 
 if __name__ == "__main__":
