c65b953c3e1e05e8cc4b3232356a4f86b925c395
Schobs <lawrenceschobs@gmail.com>
2022-01-26 13:21:27 +0000

    testing fixes

--- tests/loaddata/test_tabular_access.py
+++ tests/loaddata/test_tabular_access.py
@@ -67,8 +67,38 @@ def test_load_csv_columns_cols_return(landmark_uncertainty_dl, source_test_file,
 
     # ensure if cols_to_return is "All" that all columns are returned
     # print("LISTING DIR: ", os.listdir(landmark_uncertainty_dl))
+    if not os.path.exists(landmark_uncertainty_dl):
+        os.makedirs(landmark_uncertainty_dl)
+    filename = "Uncertainty_tuples.zip"
+    data_path = os.path.join(landmark_uncertainty_dl, filename)
+    if not os.path.exists(data_path):
+        download_file_by_url(landmark_uncertainty_url, landmark_uncertainty_dl, filename, "zip")
+
+    # download_file_by_url(landmark_uncertainty_url, landmark_uncertainty_dl, "Uncertainty_tuples.zip", "zip")
+    returned_cols = load_csv_columns(
+        (os.path.join(landmark_uncertainty_dl, source_test_file)),
+        "Testing Fold",
+        np.arange(8),
+        cols_to_return=return_columns[0],
+    )
+    assert list(returned_cols.columns) == return_columns[1]
+
+
+@pytest.mark.parametrize("source_test_file", ["PHD-Net/4CH/uncertainty_pairs_test_l0"])
+@pytest.mark.parametrize(
+    "return_columns",
+    [
+        ("All", EXPECTED_COLS),
+        ([], []),
+        ("S-MHA Error", ["S-MHA Error"]),
+        (["S-MHA Error", "E-MHA Error"], ["S-MHA Error", "E-MHA Error"]),
+    ],
+)
+def test_load_csv_columns_cols_return2(landmark_uncertainty_dl, source_test_file, return_columns):
 
     download_file_by_url(landmark_uncertainty_url, landmark_uncertainty_dl, "Uncertainty_tuples.zip", "zip")
+
+    # download_file_by_url(landmark_uncertainty_url, landmark_uncertainty_dl, "Uncertainty_tuples.zip", "zip")
     returned_cols = load_csv_columns(
         (os.path.join(landmark_uncertainty_dl, source_test_file)),
         "Testing Fold",
