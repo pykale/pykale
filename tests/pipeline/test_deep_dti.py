92c646e9a219f5ca70af77c6c09ac6d72b8b1c75
pz-white <67964033+pz-white@users.noreply.github.com>
2022-08-10 16:03:33 +0100

    Fix flake8 hook issue

--- tests/pipeline/test_deep_dti.py
+++ tests/pipeline/test_deep_dti.py
@@ -1,6 +1,6 @@
 import pytest
-import pytorch_lightning as pl
 import torch
+# import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 
 from kale.embed.seq_nn import CNNEncoder
@@ -16,8 +16,8 @@ def test_deep_data(download_path):
     subset_indices = list(range(0, 32, 2))
     test_subset = torch.utils.data.Subset(test_dataset, subset_indices)
     test_dataloader = DataLoader(dataset=test_subset, shuffle=False, batch_size=8)
-    valid_dataloader = DataLoader(dataset=test_subset, shuffle=True, batch_size=8)
-    train_dataloader = DataLoader(dataset=test_subset, shuffle=True, batch_size=4)
+    # valid_dataloader = DataLoader(dataset=test_subset, shuffle=True, batch_size=8)
+    # train_dataloader = DataLoader(dataset=test_subset, shuffle=True, batch_size=4)
     test_batch = next(iter(test_dataloader))
 
     drug_encoder = CNNEncoder(num_embeddings=64, embedding_dim=128, sequence_length=85, num_kernels=32, kernel_length=8)
@@ -28,7 +28,7 @@ def test_deep_data(download_path):
     # test deep_dta trainer
     save_parameters = {"seed": 2020, "batch_size": 256}
     model = DeepDTATrainer(drug_encoder, target_encoder, decoder, lr=0.001, ci_metric=True, **save_parameters).eval()
-    # temporarily comment off the following lines of test due to the conflict between pyg and lightning
+    # temporarily comment off the lines of test due to the conflict between pyg and lightning
     # issue PR: https://github.com/Lightning-AI/lightning/pull/14117
     # trainer = pl.Trainer(max_epochs=1, gpus=0)
     # trainer.fit(model, train_dataloaders=train_dataloader, val_dataloaders=valid_dataloader)
