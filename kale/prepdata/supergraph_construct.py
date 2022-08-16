5c8e6ffe8cadc9153f0a728356df36703389b284
nyxflower <nyx0flower@gmail.com>
2022-08-16 14:18:01 -0400

    add supervertex setting dict to supergraph

--- kale/prepdata/supergraph_construct.py
+++ kale/prepdata/supergraph_construct.py
@@ -118,20 +118,67 @@ class SuperEdge(object):
         return f"SuperEdges(\n    edge_direction={self.source_supervertex}->{self.target_supervertex}, \n    edge_index={self.edge_index.shape})"
 
 
+class SuperVertexParaSetting(object):
+    r"""Parameter settings for each supervertex.
+
+        Args:
+            supervertex_name (str): the name of the supervertex.
+            inter_feat_dim (int): the dimension of
+            the output of the internal feature layer.
+            inter_agg_dim (List[int]): the output dimensions of a sequence of internal aggregation layers.
+            exter_agg_dim (Dict[str, int], optional): the dimension of received message vector
+            from parient supervertices. Defaults to None.
+            mode (str, optinal): the allowed gripnet mode--'cat' or 'add'. Defaults to None.
+            num_bases (int, optional): Number of bases used for basis-decomposition if the supervertex is multi-relational. Defaults to 32.
+            if_catout (bool, optional): if concatenate the output of each layers. Defaults to True.
+        """
+
+    def __init__(
+        self,
+        supervertex_name: str,
+        inter_feat_dim: int,
+        inter_agg_dim: List[int],
+        exter_agg_dim: Dict[str, int] = None,
+        mode: str = None,
+        num_bases: int = 32,
+        if_catout: bool = True,
+    ) -> None:
+        self.supervertex_name = supervertex_name
+        self.inter_feat_dim = inter_feat_dim
+        self.inter_agg_dim = inter_agg_dim
+        self.mode = mode
+        self.num_bases = num_bases
+        self.if_catout = if_catout
+        self.exter_agg_dim = exter_agg_dim
+
+        # check if the mode is valid
+        if self.mode is not None and self.mode not in ["cat", "add"]:
+            error_msg = "The mode is not valid. It should be 'cat' or 'add'."
+            logging.error(error_msg)
+            raise ValueError(error_msg)
+
+
 class SuperGraph(object):
     r"""
-    The supergraph structure in GripNet. Each supergraph is a directed acyclic graph (DAG) containing 
+    The supergraph structure in GripNet. Each supergraph is a directed acyclic graph (DAG) containing
     supervertices and superedges.
 
     Args:
         supervertex_list (list[SuperVertex]): a list of supervertices.
         superedge_list (list[SuperEdge]): a list of superedges.
+        supervertex_para_setting (dict[str, SuperVertexParaSetting], Optional): the parameter settings for each supervertex.
     """
 
-    def __init__(self, supervertex_list: List[SuperVertex], superedge_list: List[SuperEdge]) -> None:
+    def __init__(
+        self,
+        supervertex_list: List[SuperVertex],
+        superedge_list: List[SuperEdge],
+        supervertex_setting_dict: Dict[str, SuperVertexParaSetting] = None,
+    ) -> None:
 
         self.supervertex_dict = {sv.name: sv for sv in supervertex_list}
         self.superedge_dict = {se.direction: se for se in superedge_list}
+        self.supervertex_setting_dict = supervertex_setting_dict
 
         self.__process_supergraph__()
         self.__update_supervertex__()
@@ -170,45 +217,13 @@ class SuperGraph(object):
             if sv.in_supervertex_list:
                 sv.if_start_supervertex = False
 
-    def __repr__(self) -> str:
-        return f"SuperGraph(\n  svertex_dict={self.supervertex_dict.values()}, \n  sedge_dict={self.superedge_dict.values()}, \n  G={self.G}), \n  topological_order={self.topological_order}"
-
-
-class SuperVertexParaSetting(object):
-    r"""Parameter settings for each supervertex.
+    def set_supergraph_para_setting(self, supervertex_setting_list: List[SuperVertexParaSetting]):
+        """Set the parameter settings of the supergraph
 
         Args:
-            supervertex_name (str): the name of the supervertex.
-            inter_feat_dim (int): the dimension of 
-            the output of the internal feature layer.
-            inter_agg_dim (List[int]): the output dimensions of a sequence of internal aggregation layers.
-            exter_agg_dim (Dict[str, int], optional): the dimension of received message vector 
-            from parient supervertices. Defaults to None.
-            mode (str, optinal): the allowed gripnet mode--'cat' or 'add'. Defaults to None.
-            num_bases (int, optional): Number of bases used for basis-decomposition if the supervertex is multi-relational. Defaults to 32.
-            if_catout (bool, optional): if concatenate the output of each layers. Defaults to True.
+            supervertex_setting_list (list[SuperVertexParaSetting]): a list of parameter settings for each supervertex.
         """
+        self.supervertex_setting_dict = {sv.supervertex_name: sv for sv in supervertex_setting_list}
 
-    def __init__(
-        self,
-        supervertex_name: str,
-        inter_feat_dim: int,
-        inter_agg_dim: List[int],
-        exter_agg_dim: Dict[str, int] = None,
-        mode: str = None,
-        num_bases: int = 32,
-        if_catout: bool = True,
-    ) -> None:
-        self.supervertex_name = supervertex_name
-        self.inter_feat_dim = inter_feat_dim
-        self.inter_agg_dim = inter_agg_dim
-        self.mode = mode
-        self.num_bases = num_bases
-        self.if_catout = if_catout
-        self.exter_agg_dim = exter_agg_dim
-
-        # check if the mode is valid
-        if self.mode is not None and self.mode not in ["cat", "add"]:
-            error_msg = "The mode is not valid. It should be 'cat' or 'add'."
-            logging.error(error_msg)
-            raise ValueError(error_msg)
+    def __repr__(self) -> str:
+        return f"SuperGraph(\n  svertex_dict={self.supervertex_dict.values()}, \n  sedge_dict={self.superedge_dict.values()}, \n  G={self.G}), \n  topological_order={self.topological_order}"
