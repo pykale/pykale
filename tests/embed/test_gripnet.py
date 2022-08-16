89f3ca6ec922ddfe1351887d4f33cc32d9af0fbf
nyxflower <nyx0flower@gmail.com>
2022-08-15 21:25:18 -0400

    add test gripnet internal module for start and end supervertices

--- tests/embed/test_gripnet.py
+++ tests/embed/test_gripnet.py
@@ -3,7 +3,8 @@ import os
 import pytest
 import torch
 
-from kale.embed.gripnet import TypicalGripNetEncoder
+from kale.embed.gripnet import GripNetInternalModule, TypicalGripNetEncoder
+from kale.prepdata.supergraph_construct import SuperEdge, SuperGraph, SuperVertex, SuperVertexParaSetting
 from kale.utils.download import download_file_by_url
 
 pose_url = "https://github.com/pykale/data/raw/main/graphs/pose_pyg_2.pt"
@@ -35,3 +36,36 @@ def test_gripnet_encoder(pose_data):
     assert gripnet.source_graph.conv_list[1].__repr__() == "GCNEncoderLayer(16, 16)"
     assert gripnet.s2t_graph.conv.__repr__() == "GCNEncoderLayer(64, 16)"
     assert gripnet.target_graph.conv_list[0].__repr__() == "RGCNEncoderLayer(48, 16, num_relations=854)"
+
+
+# create three supervertices
+node_feat = torch.randn(4, 20)
+edge_index = torch.tensor([[0, 1, 2, 3], [1, 2, 3, 4]])
+edge_type = torch.tensor([0, 0, 1, 1])
+
+supervertex1 = SuperVertex("1", node_feat, edge_index)
+supervertex2 = SuperVertex("2", node_feat, edge_index, edge_type)
+supervertex3 = SuperVertex("3", node_feat, edge_index, edge_type)
+
+# determine the supervertices among them
+edge_index = torch.tensor([[0, 1, 2, 3], [1, 1, 3, 3]])
+
+superedge1 = SuperEdge(supervertex1.name, supervertex3.name, edge_index)
+superedge2 = SuperEdge(supervertex2.name, supervertex3.name, edge_index)
+
+# create a supergraph
+supergraph = SuperGraph([supervertex1, supervertex2, supervertex3], [superedge1, superedge2])
+
+
+def test_gripnet_internal_module():
+    """GripNet Internal Module Test with two kinds of supervertices: start supervertex and end supervertex"""
+
+    setting1 = SuperVertexParaSetting("start_svertex", 20, [10, 10])
+    _ = GripNetInternalModule(
+        supervertex1.n_node_feat, supervertex1.n_edge_type, supervertex1.if_start_supervertex, setting1
+    )
+
+    setting2 = SuperVertexParaSetting("task_svertex", 20, [15, 10], exter_agg_dim={"1": 20, "2": 20}, mode="cat")
+    _ = GripNetInternalModule(
+        supervertex3.n_node_feat, supervertex3.n_edge_type, supervertex3.if_start_supervertex, setting2
+    )
