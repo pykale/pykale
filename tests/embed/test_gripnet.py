e16a026e6d79417ee3682ad6fc4f8c16787c8ba9
nyxflower <nyx0flower@gmail.com>
2022-08-18 14:36:25 -0400

    update gripnet test

--- tests/embed/test_gripnet.py
+++ tests/embed/test_gripnet.py
@@ -38,77 +38,27 @@ def test_gripnet_encoder(pose_data):
     assert gripnet.target_graph.conv_list[0].__repr__() == "RGCNEncoderLayer(48, 16, num_relations=854)"
 
 
-# create three supervertices
-node_feat = torch.randn(4, 20)
-edge_index = torch.tensor([[0, 1, 2, 3], [1, 2, 3, 3]], dtype=torch.long)
-edge_type = torch.tensor([0, 0, 1, 1], dtype=torch.long)
-
-supervertex1 = SuperVertex("1", node_feat, edge_index)
-supervertex2 = SuperVertex("2", node_feat, edge_index, edge_type)
-supervertex3 = SuperVertex("3", node_feat, edge_index, edge_type)
-
-# determine the superedges among them
-edge_index = torch.tensor([[0, 1, 2, 3], [1, 1, 3, 3]])
-
-superedge1 = SuperEdge(supervertex1.name, supervertex3.name, edge_index)
-superedge2 = SuperEdge(supervertex2.name, supervertex3.name, edge_index)
-
-# create a supergraph
-supergraph = SuperGraph([supervertex1, supervertex2, supervertex3], [superedge1, superedge2])
-
-
-# def test_gripnet_internal_module1():
-#     """GripNet Internal Module Test for start supervertex"""
-
-#     setting1 = SuperVertexParaSetting("start_supervertex", 20, [10, 10])
-#     inter_module1 = GripNetInternalModule(
-#         supervertex1.num_node_feat, supervertex1.num_edge_type, supervertex1.start_supervertex, setting1
-#     )
-
-#     x = torch.randn(20, 20)
-#     y = inter_module1(x, supervertex3.edge_index)
-
-#     assert y.shape[1] == inter_module1.out_channels
-
-
-# def test_gripnet_internal_module2():
-#     """GripNet Internal Module Test for end supervertex"""
-
-#     setting2 = SuperVertexParaSetting(
-#         "task_supervertex", 20, [15, 10], external_agg_channels_dict={"1": 20, "2": 20}, mode="cat"
-#     )
-#     inter_module2 = GripNetInternalModule(
-#         supervertex3.num_node_feat, supervertex3.num_edge_type, supervertex3.start_supervertex, setting2
-#     )
-
-#     x = torch.randn(20, 20)
-#     x1 = torch.matmul(x, inter_module2.embedding)
-#     x2 = torch.randn(20, 40)
-#     xx = torch.cat((x1, x2), dim=1)
-
-#     range_list = torch.LongTensor([[0, 2], [2, 4]])
-#     edge_weight = torch.randn(4)
-
-#     y = inter_module2(xx, supervertex3.edge_index, supervertex3.edge_type, range_list, edge_weight)
-
-#     assert y.shape[1] == inter_module2.out_channels
-
-
-# def test_gripnet_external_module():
-#     """GripNet External Module Test"""
-
-#     exter_module = GripNetExternalModule(8, 7, 5)
-#     x = torch.randn((4, 8))
-#     edge_index = torch.tensor([[0, 1, 2, 3], [1, 4, 3, 4]])
-#     y = exter_module(x, edge_index)
-
-#     assert y.shape[0] == 5
-#     assert y.shape[1] == 7
-
-
 @pytest.mark.parametrize("mode,test_in_channels,test_out_channels", [("cat", 90, 115), ("add", 30, 55)])
 def test_gripnet(mode, test_in_channels, test_out_channels):
-    """GripNet Test"""
+    """GripNet and SuperGraph Test"""
+    # create three supervertices
+    node_feat = torch.randn(4, 20)
+    edge_index = torch.tensor([[0, 1, 2, 3], [1, 2, 3, 3]], dtype=torch.long)
+    edge_type = torch.tensor([0, 0, 1, 1], dtype=torch.long)
+
+    supervertex1 = SuperVertex("1", node_feat, edge_index)
+    supervertex2 = SuperVertex("2", node_feat, edge_index, edge_type)
+    supervertex3 = SuperVertex("3", node_feat, edge_index, edge_type)
+
+    # determine the superedges among them
+    edge_index = torch.tensor([[0, 1, 2, 3], [1, 1, 3, 3]])
+
+    superedge1 = SuperEdge(supervertex1.name, supervertex3.name, edge_index)
+    superedge2 = SuperEdge(supervertex2.name, supervertex3.name, edge_index)
+
+    # create a supergraph
+    supergraph = SuperGraph([supervertex1, supervertex2, supervertex3], [superedge1, superedge2])
+
     setting1 = SuperVertexParaSetting("1", 20, [10, 10])
     setting2 = SuperVertexParaSetting("2", 20, [10, 10])
     setting3 = SuperVertexParaSetting("3", 30, [15, 10], external_agg_channels_dict={"1": 30, "2": 30}, mode=mode)
@@ -130,4 +80,6 @@ def test_gripnet(mode, test_in_channels, test_out_channels):
         assert gripnet.out_embed_dict["2"].shape[1] == 20 + 10 + 10, error_message
 
     # general tests
+    assert supervertex1.__repr__()
+    assert superedge1.__repr__()
     assert gripnet.__repr__() is not None
