ae1428a64df4a6f0d34fc346308d1522c0445638
Haolin Wang <93381130+HaolinWang2001@users.noreply.github.com>
2023-03-29 14:06:03 +0100

    Move model setup to model.py, update main.py

--- examples/cifar_cnntransformer/main.py
+++ examples/cifar_cnntransformer/main.py
@@ -1,20 +1,17 @@
+
+
 """
 This example demonstrates the use of a CNN and a Transformer-Encoder
 for image classification on CIFAR10.
-
 Reference: See kale.embed.attention_cnn for more details.
 """
-import argparse
-import os
 
-import torch
+import argparse
+
 from config import get_cfg_defaults
 from model import get_model
-from torchsummary import summary
-from trainer import CNNTransformerTrainer
 import pytorch_lightning as pl
 
-import kale.utils.logger as logging
 import kale.utils.seed as seed
 from kale.loaddata.image_access import get_cifar
 
@@ -22,14 +19,12 @@ from kale.loaddata.image_access import get_cifar
 def arg_parse():
     parser = argparse.ArgumentParser(description="PyTorch CIFAR10 Training")
     parser.add_argument("--cfg", required=True, help="path to config file", type=str)
-    # parser.add_argument('--output', default='default', help='folder to save output', type=str)
     parser.add_argument(
         "--gpus",
         default=1,
         help="gpu id(s) to use. None/int(0) for cpu. list[x,y] for xth, yth GPU."
              "str(x) for the first x GPUs. str(-1)/int(-1) for all available GPUs",
     )
-    #parser.add_argument("--resume", default="", type=str)
     args = parser.parse_args()
     return args
 
@@ -37,47 +32,27 @@ def arg_parse():
 def main():
     args = arg_parse()
 
-    # ---- setup device ----
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print("==> Using device " + device)
-
     # ---- setup config ----
     cfg = get_cfg_defaults()
     cfg.merge_from_file(args.cfg)
     cfg.freeze()
     seed.set_seed(cfg.SOLVER.SEED)
 
-    # ---- setup logger ----
-    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-    logger = logging.construct_logger("context_cnns", cfg.OUTPUT_DIR)
-    logger.info(f"Using {device}")
-    logger.info("\n" + cfg.dump())
-
     # ---- setup dataset ----
     train_loader, valid_loader = get_cifar(cfg)
 
     # ---- setup model ----
     print("==> Building model..")
-    net = get_model(cfg)
-    net = net.to(device)
-
-    model_stats = summary(net, (3, 32, 32))
-    logger.info("\n" + str(model_stats))
-
+    model, optim = get_model(cfg)
+ 
 
     # ---- setup trainers ----
-    optim = torch.optim.SGD(
-        net.parameters(), lr=cfg.SOLVER.BASE_LR, momentum=cfg.SOLVER.MOMENTUM, weight_decay=cfg.SOLVER.WEIGHT_DECAY
-    )
-    
-    model = CNNTransformerTrainer(model=net, optim=optim.state_dict(), cfg=cfg)
     trainer = pl.Trainer(
         default_root_dir=cfg.OUTPUT_DIR,
         max_epochs=cfg.SOLVER.MAX_EPOCHS,
         accelerator="auto", 
-        strategy="ddp"
-        #devices=1,
-        #gpus=args.gpus
+        strategy="ddp",
+        log_every_n_steps=1,
     )
     
 
