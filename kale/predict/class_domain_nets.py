1200bae896db53e09166ddd5463eb1ce4c10192b
Haolin Wang <93381130+HaolinWang2001@users.noreply.github.com>
2023-03-29 14:09:32 +0100

    Add ClassNet

--- kale/predict/class_domain_nets.py
+++ kale/predict/class_domain_nets.py
@@ -59,6 +59,29 @@ class SoftmaxNet(nn.Module):
         return self._n_classes
 
 
+#Previously PredictionHead
+class ClassNet(nn.Module):
+    """
+    Simple classification prediction-head block to plug ontop of the 4D
+    output of a CNN.
+    Args:
+        num_classes: the number of different classes that can be predicted.
+        input_shape: the shape that input to this head will have. Expected
+                      to be (batch_size, channels, height, width)
+    """
+
+    def __init__(self, num_classes, input_shape):
+        super(PredictionHead, self).__init__()
+        self.avgpool = nn.AvgPool2d(input_shape[2])
+        self.linear = nn.Linear(input_shape[1], num_classes)
+
+    def forward(self, x):
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.linear(x)
+        return F.log_softmax(x, 1)
+    
+    
 # Previously DataClassifierDigits
 class ClassNetSmallImage(nn.Module):
     """Regular classifier network for small-size images
