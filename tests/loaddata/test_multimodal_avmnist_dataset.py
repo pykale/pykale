a3b34d61524f76e24e79e54b34f9295e3ade725d
Mdnaimulislam <mnisuvon1@sheffield.ac.uk>
2023-04-24 15:36:19 +0100

    Fixed test_multimodal_avmnist_dataset file

--- tests/loaddata/test_multimodal_avmnist_dataset.py
+++ tests/loaddata/test_multimodal_avmnist_dataset.py
@@ -1,14 +1,51 @@
+import os
+import tempfile
+
+import numpy as np
+import pytest
+
 from kale.loaddata.avmnist_datasets import AVMNISTDataset
-from kale.utils.download import download_file_gdrive
 
 
-def test_avmnist_dataset():
-    GDRIVE_ID = "1N5k-LvLwLbPBgn3GdVg6fXMBIR6pYrKb"
-    ROOT = "avmnist"
-    DATASET_NAME = "data.zip"
-    FILE_FORMAT = "zip"
-    download_file_gdrive(GDRIVE_ID, ROOT, DATASET_NAME, FILE_FORMAT)
-    dataset = AVMNISTDataset(data_dir=ROOT)
+@pytest.fixture(scope="module")
+def dummy_avmnist_data():
+    # Create temporary directory for data
+    temp_dir = tempfile.TemporaryDirectory()
+    data_dir = temp_dir.name
+
+    # Create dummy AVMNIST data
+    create_dummy_avmnist_data(data_dir)
+
+    yield data_dir
+
+    # Clean up temporary directory
+    temp_dir.cleanup()
+
+
+def create_dummy_avmnist_data(data_dir):
+    os.makedirs(os.path.join(data_dir, "image"))
+    os.makedirs(os.path.join(data_dir, "audio"))
+
+    train_image_data = np.random.rand(100, 28, 28)
+    train_audio_data = np.random.rand(100, 112, 112)
+    train_labels = np.random.randint(0, 10, size=(100,))
+
+    test_image_data = np.random.rand(20, 28, 28)
+    test_audio_data = np.random.rand(20, 112, 112)
+    test_labels = np.random.randint(0, 10, size=(20,))
+
+    np.save(os.path.join(data_dir, "image/train_data.npy"), train_image_data)
+    np.save(os.path.join(data_dir, "audio/train_data.npy"), train_audio_data)
+    np.save(os.path.join(data_dir, "train_labels.npy"), train_labels)
+
+    np.save(os.path.join(data_dir, "image/test_data.npy"), test_image_data)
+    np.save(os.path.join(data_dir, "audio/test_data.npy"), test_audio_data)
+    np.save(os.path.join(data_dir, "test_labels.npy"), test_labels)
+
+
+def test_avmnist_dataset(dummy_avmnist_data):
+    # Initialize AVMNISTDataset with temporary data directory
+    dataset = AVMNISTDataset(dummy_avmnist_data)
 
     # Test train, validation, and test data loaders
     train_loader = dataset.get_train_loader()
