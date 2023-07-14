f21ee302b4d560af61acb9c141ea6dcfae64dbd0
Sina Tabakhi <sina.tabakhi@gmail.com>
2023-07-14 12:09:12 +0100

    Modify the mogonet loaddata tests

--- tests/loaddata/test_multiomics_datasets.py
+++ tests/loaddata/test_multiomics_datasets.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 import torch
 from torch_geometric.data import Data, DataLoader
@@ -8,17 +9,25 @@ from kale.prepdata.tabular_transform import ToOneHotEncoding, ToTensor
 from kale.utils.seed import set_seed
 
 
-def test_multiomics_datasets():
+@pytest.mark.parametrize('random_split', [True, False])
+def test_multiomics_datasets(random_split):
     num_modalities = 3
     num_classes = 2
     url = "https://github.com/pykale/data/raw/main/multiomics/ROSMAP.zip"
-    root = "tests/test_data/multiomics/"
+
+    if random_split:
+        root = "tests/test_data/multiomics/random/"
+        set_seed(2023)
+    else:
+        root = "tests/test_data/multiomics/pre_split/"
+
     file_names = []
     for modality in range(1, num_modalities + 1):
         file_names.append(f"{modality}_tr.csv")
         file_names.append(f"{modality}_lbl_tr.csv")
-        file_names.append(f"{modality}_te.csv")
-        file_names.append(f"{modality}_lbl_te.csv")
+        if not random_split:
+            file_names.append(f"{modality}_te.csv")
+            file_names.append(f"{modality}_lbl_te.csv")
 
     dataset = MultiomicsDataset(
         root=root,
@@ -26,7 +35,7 @@ def test_multiomics_datasets():
         num_classes=num_classes,
         url=url,
         raw_file_names=file_names,
-        random_split=False,
+        random_split=random_split,
         train_size=0.7,
         pre_transform=ToTensor(dtype=torch.float),
         target_pre_transform=ToOneHotEncoding(dtype=torch.float),
@@ -55,55 +64,6 @@ def test_multiomics_datasets():
         assert len(data.train_idx) > 0
         assert len(data.train_idx) == data.num_train
         assert len(data.test_idx) > 0
-        assert len(data.test_idx) == data.num_test
-
-    for modality in range(num_modalities):
-        assert dataset.get(modality).x.dtype == torch.float
-        assert dataset.get(modality).y.dtype == torch.float
-
-
-def test_multiomics_datasets_random_split():
-    set_seed(2023)
-    num_modalities = 3
-    num_classes = 2
-    url = "https://github.com/pykale/data/raw/main/multiomics/ROSMAP.zip"
-    root = "tests/test_data/multiomics/random/"
-    file_names = []
-    for modality in range(1, num_modalities + 1):
-        file_names.append(f"{modality}_tr.csv")
-        file_names.append(f"{modality}_lbl_tr.csv")
-
-    dataset = MultiomicsDataset(
-        root=root,
-        num_modalities=num_modalities,
-        num_classes=num_classes,
-        url=url,
-        raw_file_names=file_names,
-        random_split=True,
-        train_size=0.7,
-        pre_transform=ToTensor(dtype=torch.float),
-        target_pre_transform=ToOneHotEncoding(dtype=torch.float),
-    )
-
-    # Test download method
-    assert os.path.isfile(os.path.join(root, "raw/ROSMAP.zip"))
-    assert os.path.isfile(os.path.join(root, "processed/data.pt"))
-
-    for modality in range(num_modalities * 4):
-        assert os.path.exists(dataset.raw_paths[0])
-
-    # Test process method
-    assert dataset.len() == num_modalities
-    for modality in range(num_modalities):
-        data = dataset.get(modality)
-        assert isinstance(data, Data)
-        assert len(data.x) == data.num_train + data.num_test
-        assert len(data.y) == data.num_train + data.num_test
-        assert len(data.train_idx) > 0
-        assert abs(len(data.train_idx) - 0.7 * len(data.y)) <= 2
-        assert len(data.train_idx) == data.num_train
-        assert len(data.test_idx) > 0
-        assert abs(len(data.test_idx) - 0.3 * len(data.y)) <= 2
         assert len(data.test_idx) == data.num_test
 
     for modality in range(num_modalities):
