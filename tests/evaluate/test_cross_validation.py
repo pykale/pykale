0d11bbff4d1104f0a1f3a8a3d8d08edd434b98b8
Alan Thomas <athms779@gmail.com>
2023-09-20 12:12:11 +0100

    Refactor test file

--- tests/evaluate/test_cross_validation.py
+++ tests/evaluate/test_cross_validation.py
@@ -16,26 +16,34 @@ def sample_data():
 
 
 # Checks leave-one-group-out results for above sample data
-def check_leave_one_group_out_results(results):
+def check_leave_one_group_out_results(sample_data, estimator, domain_adaptation=False):
+    x, y, groups = sample_data
+    results = cross_validation.leave_one_group_out(x, y, groups, estimator, domain_adaptation)
+
+    # Check if all keys are present in the result dictionary
     assert "Target" in results
     assert "Num_samples" in results
     assert "Accuracy" in results
+
+    # Check if the length of 'Target', 'Num_samples', and 'Accuracy' lists is the same
     assert len(results["Target"]) == len(results["Num_samples"]) == len(results["Accuracy"])
+
+    # Check if the last element of 'Target' is "Average"
     assert results["Target"] == ["A", "B", "Average"]
+
+    # Check if the number of samples is correct for each target group
     assert results["Num_samples"] == [4, 4, 8]
+
+    # Check if the accuracy scores are within the expected range
     for accuracy in results["Accuracy"]:
         assert 0 <= accuracy <= 1
 
 
 def test_leave_one_group_out_without_domain_adaptation(sample_data):
-    x, y, groups = sample_data
     estimator = DummyClassifier()
-    results = cross_validation.leave_one_group_out(x, y, groups, estimator, domain_adaptation=False)
-    check_leave_one_group_out_results(results)
+    check_leave_one_group_out_results(sample_data, estimator, domain_adaptation=False)
 
 
 def test_leave_one_group_out_with_domain_adaptation(sample_data):
-    x, y, groups = sample_data
     estimator = CoIRLS(kernel="linear", lambda_=1.0, alpha=1.0)
-    results = cross_validation.leave_one_group_out(x, y, groups, estimator, domain_adaptation=True)
-    check_leave_one_group_out_results(results)
+    check_leave_one_group_out_results(sample_data, estimator, domain_adaptation=True)
