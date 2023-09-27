3fda9b16fc8404f8a867cbb3ece898a211ca0532
shuo-zhou <sz144@outlook.com>
2023-09-27 12:04:50 +0100

    update LOGO

--- kale/evaluate/cross_validation.py
+++ kale/evaluate/cross_validation.py
@@ -13,21 +13,25 @@ def leave_one_group_out(x, y, groups, estimator, domain_adaptation=False) -> dic
     Perform leave one group out cross validation for a given estimator.
 
     Args:
-        x: Input data [n_samples, n_features].
-        y: Target labels [n_samples].
-        groups: Group labels to be left out [n_samples].
-        estimator: Machine learning estimator to be evaluated from kale or scikit-learn.
-        domain_adaptation: Whether to use domain adaptation during training.
+        x (np.ndarray or torch.tensor): Input data [n_samples, n_features].
+        y (np.ndarray or torch.tensor): Target labels [n_samples].
+        groups (np.ndarray or torch.tensor): Group labels to be left out [n_samples].
+        estimator (estimator object): Machine learning estimator to be evaluated from kale or scikit-learn.
+        domain_adaptation (bool): Whether to use domain adaptation, i.e., leveraging test data, during training.
 
     Returns:
         dict: A dictionary containing results for each target group with 3 keys.
-            - 'Target': List of unique target groups or classes.
-            - 'Num_samples': List containing number of samples in each target group.
-            - 'Accuracy': List of accuracy scores for each target group.
+            - 'Target': A list of unique target groups or classes. The final entry is "Average".
+            - 'Num_samples': A list where each entry indicates the number of samples in its corresponding target group.
+                            The final entry represents the total number of samples.
+            - 'Accuracy': A list where each entry indicates the accuracy score for its corresponding target group.
+                            The final entry represents the overall mean accuracy.
     """
     enc = OneHotEncoder(handle_unknown="ignore")
     group_mat = enc.fit_transform(groups.reshape(-1, 1)).toarray()
     target, num_samples, accuracy = np.unique(groups).tolist(), [], []
+
+    y_pred = np.zeros(y.shape)
 
     for train, test in LeaveOneGroupOut().split(x, y, groups=groups):
         x_src, x_tgt = x[train], x[test]
@@ -38,14 +42,13 @@ def leave_one_group_out(x, y, groups, estimator, domain_adaptation=False) -> dic
         else:
             estimator.fit(x_src, y_src)
 
-        y_pred = estimator.predict(x_tgt)
+        y_pred[test] = estimator.predict(x_tgt)
         num_samples.append(x_tgt.shape[0])
-        accuracy.append(accuracy_score(y_tgt, y_pred))
+        accuracy.append(accuracy_score(y_tgt, y_pred[test]))
 
-    mean_acc = sum([num_samples[i] * accuracy[i] for i in range(len(target))]) / x.shape[0]
     target.append("Average")
     num_samples.append(x.shape[0])
-    accuracy.append(mean_acc)
+    accuracy.append(accuracy_score(y, y_pred))
 
     return {
         "Target": target,
