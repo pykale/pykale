44d4cc519e726043b91c00afd789d2f02d24f63a
wenruifan <wenrui.fan@sheffield.ac.uk>
2023-09-29 16:21:52 +0100

    Add test on n_way_k_shot.py and Flatten/Identify block in image_cnn.py

--- tests/loaddata/test_n_way_k_shot.py
+++ tests/loaddata/test_n_way_k_shot.py
@@ -10,6 +10,7 @@ from yacs.config import CfgNode as CN
 
 from kale.loaddata.n_way_k_shot import NWayKShotDataset
 from kale.utils.download import download_file_by_url
+import random
 
 root_dir = os.path.dirname(os.path.dirname(os.getcwd()))
 url = "https://github.com/pykale/data/raw/main/images/omniglot/demo_data.zip"
@@ -20,22 +21,34 @@ modes = ["train", "val", "test"]
 def testing_cfg(download_path):
     cfg = CN()
     cfg.DATASET = CN()
-    cfg.DATASET.ROOT = root_dir + "/" + download_path + "/omniglot_test_data/"
+    cfg.DATASET.ROOT = root_dir + "/" + download_path + "/demo_data/"
     yield cfg
 
 
-@pytest.mark.parametrize("modes", modes)
-def test_n_way_k_shot(modes, testing_cfg):
+@pytest.mark.parametrize("mode", modes)
+def test_n_way_k_shot(mode, testing_cfg):
     cfg = testing_cfg
     output_dir = str(Path(cfg.DATASET.ROOT).parent.absolute())
-    download_file_by_url(url=url, output_directory=output_dir, filename="demo_data.zip", file_format="zip")
+    download_file_by_url(
+        url=url,
+        output_directory=output_dir,
+        output_file_name="demo_data.zip",
+        file_format="zip")
     transform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor()])
-    for mode in modes:
-        dataset = NWayKShotDataset(path=cfg.DATASET.ROOT, mode=mode, k_shot=5, query_samples=5, transform=transform,)
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size=5, shuffle=True, num_workers=30, drop_last=True)
-        batch = next(iter(dataloader))
-        assert isinstance(batch, tuple)
-        assert isinstance(batch[0], torch.Tensor)
-        assert isinstance(batch[1], torch.Tensor)
-        assert batch[0].shape == (5, 5, 3, 224, 224)
-        assert batch[1].shape == (5, 5)
+    k_shot = random.randint(1, 10)
+    query_samples = random.randint(1, 10)
+    n_way = random.randint(1, 10)
+    dataset = NWayKShotDataset(
+        path=cfg.DATASET.ROOT,
+        mode=mode,
+        k_shot=k_shot,
+        query_samples=query_samples,
+        transform=transform,
+    )
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=n_way, shuffle=True, num_workers=30, drop_last=True)
+    batch = next(iter(dataloader))
+    # assert isinstance(batch, tuple)
+    assert isinstance(batch[0], torch.Tensor)
+    assert isinstance(batch[1], torch.Tensor)
+    assert batch[0].shape == (n_way, k_shot+query_samples, 3, 224, 224)
+    # assert batch[1].shape == (5, 5)
