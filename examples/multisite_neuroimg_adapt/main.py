d47c5d1521a3da29351c08f0097387496a059824
Alan Thomas <athms779@gmail.com>
2023-10-03 16:56:13 +0100

    Check codecov

--- examples/multisite_neuroimg_adapt/main.py
+++ examples/multisite_neuroimg_adapt/main.py
@@ -78,7 +78,7 @@ def main():
     brain_networks = correlation_measure.fit_transform(time_series)
 
     # ---- Machine Learning for Multi-site Data ----
-    print("Baseline Model")
+    print("Baseline")
     estimator = RidgeClassifier()
     results = cross_validation.leave_one_group_out(
         brain_networks, pheno["DX_GROUP"].values, pheno["SITE_ID"].values, estimator
