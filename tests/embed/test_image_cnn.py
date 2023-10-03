16f4841a4aaa999718bbc8ae8f6eae220a6b8d6f
Wenrui-Fan <wenrui.fan@sheffield.ac.uk>
2023-10-03 20:46:51 +0100

    Fix typos in annotations.

--- tests/embed/test_image_cnn.py
+++ tests/embed/test_image_cnn.py
@@ -50,8 +50,6 @@ def test_shapes(param):
     model.eval()
     output_batch = model(INPUT_BATCH)
     assert output_batch.size() == (BATCH_SIZE, out_size)
-    # logging.info("Output shape: {}".format(model.output_size()))
-    # assert model.output_size() == out_size
 
 
 def test_lenet_output_shapes():
@@ -59,7 +57,6 @@ def test_lenet_output_shapes():
     output_channels = 6
     additional_layers = 2
     lenet = LeNet(input_channels, output_channels, additional_layers)
-
     x = torch.randn(16, 3, 32, 32)
     output = lenet(x)
     assert output.shape == (16, 24, 4, 4), "Unexpected output shape"
