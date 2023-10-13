85a74da902c6014dd838006342d154a96fd12aea
Haolin Wang <lwang0101@outlook.com>
2023-10-13 19:59:57 +0100

    code format update

--- tests/helpers/pipe_test_helper.py
+++ tests/helpers/pipe_test_helper.py
@@ -10,7 +10,10 @@ class ModelTestHelper:
         # training process
         trainer = pl.Trainer(
             default_root_dir="tests/outputs",
-            min_epochs=train_params["nb_init_epochs"], max_epochs=train_params["nb_adapt_epochs"], devices=1, **kwargs
+            min_epochs=train_params["nb_init_epochs"],
+            max_epochs=train_params["nb_adapt_epochs"],
+            devices=1,
+            **kwargs,
         )
         trainer.fit(model)
         trainer.test()
