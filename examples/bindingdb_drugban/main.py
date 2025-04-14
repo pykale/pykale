c3a646e01b37b652d6ed9fd7be6ba6c3a0d9cc1e
jiayang-zhang <jiayang.zhang@icloud.com>
2025-04-14 19:01:58 +0100

    add files for DrugBAN example, replacing DGL with PyG

---
+++ examples/bindingdb_drugban/main.py
@@ -0,0 +1,263 @@
+# =============================================================================
+# Author: Jiayang Zhang, jiayang.zhang@sheffield.ac.uk
+# =============================================================================
+
+"""
+DrugBAN on BindingDB/BioSNAP/Human drug-protein interaction prediction
+
+Reference: https://github.com/peizhenbai/DrugBAN/blob/main/main.py
+"""
+
+import argparse
+import os
+import sys
+import warnings
+from time import time
+
+import pandas as pd
+import torch
+from comet_ml import Experiment
+from configs import get_cfg_defaults
+from torch.utils.data import DataLoader
+
+sys.path.append("/home/jiang/PycharmProjects/pykale")
+from kale.embed.ban import DrugBAN
+from kale.loaddata.drugban_datasets import DTIDataset, graph_collate_func, MultiDataLoader
+from kale.pipeline.drugban_trainer import Trainer
+from kale.predict.class_domain_nets import Discriminator
+from kale.utils.seed import set_seed
+
+
+def arg_parse():
+    """Parsing arguments"""
+    parser = argparse.ArgumentParser(description="DrugBAN for DTI prediction")
+    parser.add_argument("--cfg", required=True, help="path to config file", type=str)
+    parser.add_argument("--data", required=True, type=str, metavar="TASK", help="dataset")
+    parser.add_argument(
+        "--split", default="random", type=str, metavar="S", help="split task", choices=["random", "cold", "cluster"]
+    )
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    """The main for this DrugBAN example, showing the workflow"""
+
+    # ---- setup device ----
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    torch.cuda.empty_cache()
+
+    # ---- ignore warnings ----
+    warnings.filterwarnings("ignore", message="invalid value encountered in divide")
+
+    # ---- get args ----
+    args = arg_parse()
+
+    # ---- setup configs ----
+    cfg = get_cfg_defaults()
+    cfg.merge_from_file(args.cfg)
+
+    set_seed(cfg.SOLVER.SEED)
+    suffix = str(int(time() * 1000))[6:]
+
+    # ---- setup output directory ----
+    path = cfg.RESULT.OUTPUT_DIR
+    path = path.strip().rstrip("\\")
+    is_exists = os.path.exists(path)
+    if not is_exists:
+        os.makedirs(path)
+
+    # ---- setup comet ----
+    experiment = None
+    comet_support = True
+
+    # ---- print information ----
+    print(f"Config yaml: {args.cfg}")
+    print(f"Hyperparameters: {dict(cfg)}")
+    print(f"Running on: {device}", end="\n\n")
+
+    # ---- setup dataset ----
+    dataFolder = f"./datasets/{args.data}"
+    dataFolder = os.path.join(dataFolder, str(args.split))
+
+    if not cfg.DA.TASK:
+        """
+        'cfg.DA.TASK = False' refers to 'in-domain' splitting strategy, where
+        each experimental dataset is randomly divided into training, validation,
+        and test sets with a 7:1:2 ratio.
+        """
+        train_path = os.path.join(dataFolder, "train.csv")
+        val_path = os.path.join(dataFolder, "val.csv")
+        test_path = os.path.join(dataFolder, "test.csv")
+        df_train = pd.read_csv(train_path)
+        df_val = pd.read_csv(val_path)
+        df_test = pd.read_csv(test_path)
+
+        train_dataset = DTIDataset(df_train.index.values, df_train)
+        val_dataset = DTIDataset(df_val.index.values, df_val)
+        test_dataset = DTIDataset(df_test.index.values, df_test)
+
+    else:
+        """
+        'cfg.DA.TASK = True' refers to 'cross-domain' splitting strategy, where
+        we used the single-linkage algorithm to cluster drugs and proteins, and randomly
+        selected 60% of the drug clusters and 60% of the protein clusters.
+
+        All drug-protein pairs in the selected clusters are source domain data.
+        The remaining drug-protein pairs are target domain data.
+
+        In the setting of domain adaptation, all labelled source domain data and 80% unlabelled
+        target domain data are used for training. The remaining 20% labelled target domain data are used for testing.
+        """
+        train_source_path = os.path.join(dataFolder, "source_train.csv")
+        train_target_path = os.path.join(dataFolder, "target_train.csv")
+        test_target_path = os.path.join(dataFolder, "target_test.csv")
+        df_train_source = pd.read_csv(train_source_path)
+        df_train_target = pd.read_csv(train_target_path)
+        df_test_target = pd.read_csv(test_target_path)
+
+        train_dataset = DTIDataset(df_train_source.index.values, df_train_source)
+        train_target_dataset = DTIDataset(df_train_target.index.values, df_train_target)
+        test_target_dataset = DTIDataset(df_test_target.index.values, df_test_target)
+
+    # ---- setup comet ----
+    if cfg.COMET.USE and comet_support:
+        experiment = Experiment(
+            api_key=cfg.COMET.API_KEY,
+            project_name=cfg.COMET.PROJECT_NAME,
+            auto_output_logging="simple",
+            log_graph=True,
+            log_code=False,
+            log_git_metadata=False,
+            log_git_patch=False,
+            auto_param_logging=False,
+            auto_metric_logging=False,
+        )
+        hyper_params = {
+            "LR": cfg.SOLVER.LR,
+            "Output_dir": cfg.RESULT.OUTPUT_DIR,
+            "DA_use": cfg.DA.USE,
+            "DA_task": cfg.DA.TASK,
+        }
+        if cfg.DA.USE:
+            da_hyper_params = {
+                "DA_init_epoch": cfg.DA.INIT_EPOCH,
+                "Use_DA_entropy": cfg.DA.USE_ENTROPY,
+                "Random_layer": cfg.DA.RANDOM_LAYER,
+                "Original_random": cfg.DA.ORIGINAL_RANDOM,
+                "DA_optim_lr": cfg.SOLVER.DA_LR,
+            }
+            hyper_params.update(da_hyper_params)
+        experiment.log_parameters(hyper_params)
+        if cfg.COMET.TAG is not None:
+            experiment.add_tag(cfg.COMET.TAG)
+        experiment.set_name(f"{args.data}_{suffix}")
+
+    # ---- setup dataloader params ----
+    params = {
+        "batch_size": cfg.SOLVER.BATCH_SIZE,
+        "shuffle": True,
+        "num_workers": cfg.SOLVER.NUM_WORKERS,
+        "drop_last": True,
+        "collate_fn": graph_collate_func,
+    }
+
+    # ---- setup dataloader ----
+    if not cfg.DA.USE:
+        # If domain adaptation is not used
+        training_generator = DataLoader(train_dataset, **params)
+        params["shuffle"] = False
+        params["drop_last"] = False
+        if not cfg.DA.TASK:
+            # If in-domain splitting strategy is used
+            val_generator = DataLoader(val_dataset, **params)
+            test_generator = DataLoader(test_dataset, **params)
+        else:
+            # If cross-domain splitting strategy is used
+            val_generator = DataLoader(test_target_dataset, **params)
+            test_generator = DataLoader(test_target_dataset, **params)
+    else:
+        # If domain adaptation is used, and cross-domain splitting strategy is used
+        source_generator = DataLoader(train_dataset, **params)
+        target_generator = DataLoader(train_target_dataset, **params)
+        n_batches = max(len(source_generator), len(target_generator))
+        multi_generator = MultiDataLoader(dataloaders=[source_generator, target_generator], n_batches=n_batches)
+
+        params["shuffle"] = False
+        params["drop_last"] = False
+        val_generator = DataLoader(test_target_dataset, **params)
+        test_generator = DataLoader(test_target_dataset, **params)
+
+    # ---- setup model and optimizer----
+    model = DrugBAN(**cfg).to(device)
+
+    if cfg.DA.USE:
+        # If domain adaptation is used
+        if cfg["DA"]["RANDOM_LAYER"]:
+            # Initialize the Discriminator with an input size from the random dimension specified in the config
+            domain_dmm = Discriminator(input_size=cfg["DA"]["RANDOM_DIM"], n_class=cfg["DECODER"]["BINARY"]).to(device)
+        else:
+            # Initialize the Discriminator with an input size derived from the decoder's input dimension
+            domain_dmm = Discriminator(
+                input_size=cfg["DECODER"]["IN_DIM"] * cfg["DECODER"]["BINARY"], n_class=cfg["DECODER"]["BINARY"]
+            ).to(device)
+        # params = list(model.parameters()) + list(domain_dmm.parameters())
+
+        # Initialize the optimizer for the DrugBAN model
+        opt = torch.optim.Adam(model.parameters(), lr=cfg.SOLVER.LR)
+        # Initialize the optimizer for the Domain Discriminator model
+        opt_da = torch.optim.Adam(domain_dmm.parameters(), lr=cfg.SOLVER.DA_LR)
+    else:
+        # If domain adaptation is not used, only initialize the optimizer for the DrugBAN model
+        opt = torch.optim.Adam(model.parameters(), lr=cfg.SOLVER.LR)
+
+    torch.backends.cudnn.benchmark = True
+
+    # ---- setup trainer ----
+    if not cfg.DA.USE:
+        # If domain adaptation is not used
+        trainer = Trainer(
+            model,
+            opt,
+            device,
+            training_generator,
+            val_generator,
+            test_generator,
+            opt_da=None,
+            discriminator=None,
+            experiment=experiment,
+            **cfg,
+        )
+    else:
+        # If domain adaptation is used
+        trainer = Trainer(
+            model,
+            opt,
+            device,
+            multi_generator,
+            val_generator,
+            test_generator,
+            opt_da=opt_da,
+            discriminator=domain_dmm,
+            experiment=experiment,
+            **cfg,
+        )
+
+    # ---- train model ----
+    result = trainer.train()
+
+    # ---- save model architecture and result ----
+    with open(os.path.join(cfg.RESULT.OUTPUT_DIR, "model_architecture.txt"), "w") as wf:
+        wf.write(str(model))
+
+    print()
+    print(f"Directory for saving result: {cfg.RESULT.OUTPUT_DIR}")
+
+    return result
+
+
+if __name__ == "__main__":
+    s = time()
+    result = main()
+    e = time()
+    print(f"Total running time: {round(e - s, 2)}s")
---
+++ kale/loaddata/drugban_datasets.py
@@ -0,0 +1,217 @@
+# =============================================================================
+# Author: Jiayang Zhang, jiayang.zhang@sheffield.ac.uk
+# =============================================================================
+
+"""
+Dataset setting and data loader for BindingDB, BioSNAP and Human datasets,
+by refactoring  https://github.com/peizhenbai/DrugBAN/blob/main/dataloader.py
+"""
+
+
+import numpy as np
+import torch
+from rdkit import Chem
+from torch.utils.data import Dataset
+from torch_geometric.data import Batch, Data
+from torch_geometric.utils import add_self_loops
+
+from kale.prepdata.chem_transform import integer_label_protein
+
+
+def graph_collate_func(x):
+    d, p, y = zip(*x)
+    d = Batch.from_data_list(d)
+    return d, torch.tensor(np.array(p)), torch.tensor(y)
+
+
+def smiles_to_graph(smiles, max_drug_nodes):
+    mol = Chem.MolFromSmiles(smiles)
+
+    atom_features = []  # shape: (num_atoms, num_atom_features)
+    for atom in mol.GetAtoms():
+        atom_features.append(
+            [
+                atom.GetAtomicNum(),  # Atomic number - essential
+                atom.GetDegree(),
+                atom.GetImplicitValence(),
+                atom.GetFormalCharge(),
+                atom.GetNumRadicalElectrons(),
+                atom.GetHybridization(),
+                atom.GetIsAromatic(),
+            ]
+        )
+    atom_features = torch.tensor(atom_features, dtype=torch.float)
+
+    edge_index = []  # shape: (2, num_edges) --> each is [source, target]
+    edge_features = []  # shape: (num_edges, num_edge_features)
+    for bond in mol.GetBonds():
+        start, end = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
+        edge_index += [[start, end], [end, start]]
+        edge_features += [
+            [bond.GetBondTypeAsDouble()],
+            [bond.GetBondTypeAsDouble()],
+        ]
+
+    edge_index = torch.tensor(edge_index, dtype=torch.long).t().contiguous()
+    edge_features = torch.tensor(edge_features, dtype=torch.float)
+    # add self-loops
+    edge_index, edge_features = add_self_loops(
+        edge_index=edge_index, edge_attr=edge_features, num_nodes=atom_features.size(0), fill_value=0.0
+    )
+
+    # Make sure edge_attr is always 2D
+    if edge_features.ndim == 1:
+        edge_features = edge_features.unsqueeze(-1)
+
+    # Note: one-hot encoding is used for atom and bond features in dgl, while pyG uses float
+
+    # ======== Padding: Add virtual nodes ===========
+    num_actual_nodes = atom_features.size(0)
+    if num_actual_nodes < max_drug_nodes:
+        num_virtual_nodes = max_drug_nodes - num_actual_nodes
+
+        virtual_node_features = torch.zeros(num_virtual_nodes, atom_features.size(1))
+        atom_features = torch.cat([atom_features, virtual_node_features], dim=0)
+        # Note: No new edges are added for virtual nodes
+
+    assert atom_features.ndim == 2, f"x must be 2D, got shape {atom_features.shape}"
+    assert edge_features.ndim == 2, f"edge_attr must be 2D, got shape {edge_features.shape}"
+
+    graph = Data(x=atom_features, edge_index=edge_index, edge_attr=edge_features, num_nodes=max_drug_nodes)
+
+    return graph
+
+
+class DTIDataset(Dataset):
+    def __init__(self, list_IDs, df, max_drug_nodes=290):
+        """
+        Initializes the DTIDataset.
+
+        Parameters:
+        -----------
+        list_IDs : list
+            List of indices corresponding to the rows in the DataFrame `df` that will be used by the dataset.
+
+        df : pandas.DataFrame
+            The DataFrame containing the drug-target interaction data.
+
+        max_drug_nodes : int, optional
+            Maximum number of nodes for the molecular graphs. Default is 290.
+        """
+        self.list_IDs = list_IDs
+        self.df = df
+        self.max_drug_nodes = max_drug_nodes
+
+    def __len__(self):
+        """
+        Returns the total number of samples in the dataset.
+
+        Returns:
+        --------
+        int
+            The total number of samples.
+        """
+        return len(self.list_IDs)
+
+    def __getitem__(self, index):
+        """
+        Returns a single drug-target pair sample.
+
+        Parameters:
+        -----------
+        index : int
+            The index of the sample to retrieve.
+
+        Returns:
+        --------
+        v_d : DGLGraph
+            A tensor representing the drug molecule, with node features and optional virtual nodes.
+
+        v_p : torch.Tensor
+            A tensor representing the encoded protein sequence.
+
+        y : float
+            The label for this drug-protein pair.
+        """
+
+        # Retrieve the actual index in the DataFrame from the list of IDs
+        index = self.list_IDs[index]
+
+        # Get SMILES
+        v_d = self.df.iloc[index]["SMILES"]
+        v_d = smiles_to_graph(smiles=v_d, max_drug_nodes=self.max_drug_nodes)
+
+        # Extract the protein sequence and convert it to a tensor of integers
+        v_p = self.df.iloc[index]["Protein"]
+        v_p = integer_label_protein(v_p)
+
+        # Extract the label
+        y = self.df.iloc[index]["Y"]
+
+        return v_d, v_p, y
+
+
+# train_path = os.path.join("/home/jiang/PycharmProjects/pykale/examples/bindingdb_drugban/datasets/biosnap/random", "train.csv")
+# df_train = pd.read_csv(train_path)
+# train_dataset = DTIDataset(df_train.index.values, df_train).__getitem__(index=1)
+
+
+class MultiDataLoader(object):
+    """
+     A class to iterate over multiple DataLoader objects in parallel.
+
+
+    Args:
+        _dataloaders (list): A list of DataLoader objects.
+        _n_batches (int): The number of batches to iterate over.
+        _iterators (list): A list of iterators corresponding to the DataLoaders.
+
+    """
+
+    def __init__(self, dataloaders, n_batches):
+        """Initialise the MultiDataLoader
+
+        Args:
+            dataloaders (list): A list of DataLoader objects to iterate over.
+            n_batches (int): The number of batches to iterate. Must be greater than 0.
+
+        Raises:
+            ValueError: If n_batches is less than or equal to 0.
+        """
+        if n_batches <= 0:
+            raise ValueError("n_batches should be > 0")
+        self._dataloaders = dataloaders
+        self._n_batches = np.maximum(1, n_batches)
+        self._init_iterators()
+
+    def _init_iterators(self):
+        """Initializes iterators for each DataLoader."""
+        self._iterators = [iter(dl) for dl in self._dataloaders]
+
+    def _get_nexts(self):
+        """Get the next batch from each DataLoader."""
+
+        def _get_next_dl_batch(di, dl):
+            try:
+                batch = next(dl)
+            except StopIteration:
+                new_dl = iter(self._dataloaders[di])
+                self._iterators[di] = new_dl
+                batch = next(new_dl)
+            return batch
+
+        return [_get_next_dl_batch(di, dl) for di, dl in enumerate(self._iterators)]
+
+    def __iter__(self):
+        """Iterates over the DataLoader objects
+
+        Yields:
+            list: A list of batches, one from each DataLoader, in each iteration.
+        """
+        for _ in range(self._n_batches):
+            yield self._get_nexts()
+        self._init_iterators()
+
+    def __len__(self):
+        """Returns the number of batches"""
+        return self._n_batches
