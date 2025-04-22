b9fabf7049393af15717abb0205a486b4f55ba5d
L. M. Riza Rizky <42672299+zaRizk7@users.noreply.github.com>
2025-04-22 18:48:04 +0100

    replace 'n' to 'num' for more explicit var naming

--- tests/helpers/toy_dataset.py
+++ tests/helpers/toy_dataset.py
@@ -3,9 +3,9 @@ from sklearn.utils.validation import check_random_state
 
 
 def make_domain_shifted_dataset(
-    n_domains=3,
-    n_samples_per_class=100,
-    n_features=2,
+    num_domains=3,
+    num_samples_per_class=100,
+    num_features=2,
     class_sep=1.0,
     centroid_shift_scale=5.0,
     random_state=None,
@@ -13,22 +13,22 @@ def make_domain_shifted_dataset(
     random_state = check_random_state(random_state)
 
     # Shared linear separator
-    w = random_state.randn(n_features)
+    w = random_state.randn(num_features)
     w = w / np.linalg.norm(w)
 
     X_all = []
     y_all = []
     domain_all = []
 
-    for i_domain in range(n_domains):
-        domain_shift = random_state.randn(n_features) * centroid_shift_scale
+    for i_domain in range(num_domains):
+        domain_shift = random_state.randn(num_features) * centroid_shift_scale
 
         for label in [0, 1]:
             class_mean = (label - 0.5) * class_sep * w + domain_shift
-            cov = np.eye(n_features)
-            X_class = random_state.multivariate_normal(class_mean, cov, n_samples_per_class)
-            y_class = np.full(n_samples_per_class, label)
-            domain_class = np.full(n_samples_per_class, i_domain)
+            cov = np.eye(num_features)
+            X_class = random_state.multivariate_normal(class_mean, cov, num_samples_per_class)
+            y_class = np.full(num_samples_per_class, label)
+            domain_class = np.full(num_samples_per_class, i_domain)
 
             X_all.append(X_class)
             y_all.append(y_class)
