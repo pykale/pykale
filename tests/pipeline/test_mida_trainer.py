df9d3f5f93351458ab121a97e4922b02825bfea7
L. M. Riza Rizky <42672299+zaRizk7@users.noreply.github.com>
2025-04-29 11:19:08 +0100

    separate test cases, add testing methods and add sanity check

--- tests/pipeline/test_mida_trainer.py
+++ tests/pipeline/test_mida_trainer.py
@@ -1,16 +1,32 @@
 # import numpy as np
 import pytest
+from numpy import testing
 
 # from numpy import testing
 # from sklearn.datasets import make_blobs
 from sklearn.base import clone
+from sklearn.decomposition import PCA
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import LeaveOneGroupOut
+from sklearn.model_selection import GridSearchCV, LeaveOneGroupOut
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 
 from kale.pipeline.mida_trainer import MIDATrainer
 
 from ..helpers.toy_dataset import make_domain_shifted_dataset
+
+TRANSFORMER = [None, MinMaxScaler()]
+PARAM_GRID = [
+    {"C": [2**-5, 1, 2**15]},
+    {"C": [2**-5, 1, 2**15], "domain_adapter__mu": [2**-5, 1, 2**15]},
+    [
+        {"C": [2**-5, 1, 2**15]},
+        {"C": [2**-5, 1, 2**15], "domain_adapter__mu": [2**-5, 1, 2**15]},
+    ],
+]
+SEARCH_STRATEGY = ["random", "grid"]
+SCORING = [None, "f1", "roc_auc", ["accuracy", "f1", "roc_auc"]]
+CV = [None, 3, LeaveOneGroupOut()]
 
 
 @pytest.fixture(scope="module")
@@ -25,27 +41,49 @@ def toy_data():
         random_state=0,
     )
 
-    return x, y, domains
+    factors = OneHotEncoder(sparse_output=False).fit_transform(domains.reshape(-1, 1))
 
-
-TRANSFORMER = [None, MinMaxScaler()]
-PARAM_GRID = [
-    {"C": [2**-5, 1, 2**15]},
-    {"C": [2**-5, 1, 2**15], "domain_adapter__mu": [2**-5, 1, 2**15]},
-]
+    return x, y, domains, factors
 
 
 @pytest.mark.parametrize("transformer", TRANSFORMER)
 @pytest.mark.parametrize("param_grid", PARAM_GRID)
-@pytest.mark.parametrize("search_strategy", ["random", "grid"])
-@pytest.mark.parametrize("scoring", [None, "f1", "roc_auc", ["accuracy", "f1", "roc_auc"]])
-@pytest.mark.parametrize("cv", [None, 3, LeaveOneGroupOut()])
-def test_mida_trainer(toy_data, transformer, param_grid, search_strategy, scoring, cv):
-    x, y, domains = toy_data
+def test_params_consistency_mida_trainer(toy_data, transformer, param_grid):
+    x, y, domains, factors = toy_data
 
     param_grid = clone(param_grid, safe=False)
     if transformer is not None:
-        param_grid.update({"transformer__feature_range": [(-1, 1), (0, 1)]})
+        if isinstance(param_grid, list):
+            for i in range(len(param_grid)):
+                param_grid[i].update({"transformer__feature_range": [(-1, 1), (0, 1)]})
+        else:
+            param_grid.update({"transformer__feature_range": [(-1, 1), (0, 1)]})
+
+    trainer = MIDATrainer(
+        estimator=LogisticRegression(random_state=0, max_iter=10),
+        transformer=transformer,
+        param_grid=param_grid,
+        error_score="raise",
+        random_state=0,
+    )
+
+    trainer.fit(x, y, factors=factors, groups=domains)
+
+    # gather keys from list of param_grid
+    if isinstance(param_grid, list):
+        param_keys = set(k for d in param_grid for k in d.keys())
+    else:
+        param_keys = set(param_grid.keys())
+
+    missing_param_keys = set(trainer.best_params_.keys()) - param_keys
+
+    assert "params" in trainer.cv_results_, "Missing 'params' in cv_results_"
+    assert len(missing_param_keys) <= 0, f"Missing {missing_param_keys} in best_params_"
+
+
+@pytest.mark.parametrize("scoring", [None, "f1", "roc_auc", ["accuracy", "f1", "roc_auc"]])
+def test_scoring_support_mida_trainer(toy_data, scoring):
+    x, y, domains, factors = toy_data
 
     refit = True
     if isinstance(scoring, list):
@@ -53,26 +91,149 @@ def test_mida_trainer(toy_data, transformer, param_grid, search_strategy, scorin
 
     trainer = MIDATrainer(
         estimator=LogisticRegression(random_state=0, max_iter=10),
-        transformer=transformer,
-        param_grid=param_grid,
-        search_strategy=search_strategy,
+        param_grid=PARAM_GRID[0],
         scoring=scoring,
-        cv=cv,
-        num_iter=10,
         refit=refit,
+        error_score="raise",
     )
 
-    factors = OneHotEncoder(sparse_output=False).fit_transform(domains.reshape(-1, 1))
     trainer.fit(x, y, factors=factors, groups=domains)
 
-    assert "params" in trainer.cv_results_
-    assert all(k in param_grid for k in trainer.best_params_.keys())
-    if isinstance(scoring, list):
-        for score in scoring:
-            assert f"mean_test_{score}" in trainer.cv_results_
-            assert f"std_test_{score}" in trainer.cv_results_
-            assert f"rank_test_{score}" in trainer.cv_results_
-    else:
-        assert "mean_test_score" in trainer.cv_results_
-        assert "std_test_score" in trainer.cv_results_
-        assert "rank_test_score" in trainer.cv_results_
+    if scoring is None or isinstance(scoring, str):
+        scoring = ["score"]
+
+    for score in scoring:
+        assert f"mean_test_{score}" in trainer.cv_results_, f"Missing mean_test_{score} in cv_results_"
+        assert f"std_test_{score}" in trainer.cv_results_, f"Missing std_test_{score} in cv_results_"
+        assert f"rank_test_{score}" in trainer.cv_results_, f"Missing rank_test_{score} in cv_results_"
+
+
+@pytest.mark.parametrize("cv", [None, 3, LeaveOneGroupOut()])
+def test_cv_support_mida_trainer(toy_data, cv):
+    x, y, domains, factors = toy_data
+
+    trainer = MIDATrainer(
+        LogisticRegression(random_state=0, max_iter=10), param_grid=PARAM_GRID[0], cv=cv, error_score="raise"
+    )
+
+    trainer.fit(x, y, factors=factors, groups=domains)
+
+    if cv is None:
+        cv = 5
+    elif isinstance(cv, LeaveOneGroupOut):
+        cv = 10
+
+    assert any(
+        key.startswith(f"split{cv-1}") for key in trainer.cv_results_.keys()
+    ), f"Missing keys starting with 'split{cv-1}' in cv_results_"
+
+
+@pytest.mark.parametrize("search_strategy", ["random", "grid"])
+def test_search_strategy_support_mida_trainer(toy_data, search_strategy):
+    x, y, domains, factors = toy_data
+
+    trainer = MIDATrainer(
+        estimator=LogisticRegression(random_state=0, max_iter=10),
+        param_grid=PARAM_GRID[0],
+        search_strategy=search_strategy,
+        error_score="raise",
+    )
+    trainer.fit(x, y, factors=factors, groups=domains)
+
+    # assert len(trainer.)
+
+
+def test_fit_and_methods_mida_trainer(toy_data):
+    x, y, domains, factors = toy_data
+
+    param_grid = clone(PARAM_GRID[1], safe=False)
+    param_grid.update(
+        {
+            "transformer__feature_range": [(-1, 1), (0, 1)],
+            "domain_adapter__num_components": [10],
+            "domain_adapter__fit_inverse_transform": [True],
+        }
+    )
+
+    trainer = MIDATrainer(
+        estimator=LogisticRegression(random_state=0, max_iter=10),
+        transformer=MinMaxScaler(),
+        param_grid=param_grid,
+        error_score="raise",
+    )
+
+    trainer.fit(x, y, factors=factors, groups=domains)
+
+    # test adaptation (excluding estimator)
+    x_transformed = trainer.adapt(x, factors=factors)
+    testing.assert_array_equal((len(x), 10), x_transformed.shape)
+
+    # test predict
+    y_pred = trainer.predict(x)
+    testing.assert_array_equal((len(x),), y_pred.shape)
+
+    # test predict_proba
+    y_proba = trainer.predict_proba(x)
+    testing.assert_array_equal((len(x), 2), y_proba.shape)
+
+    # test predict_log_proba
+    y_log_proba = trainer.predict_log_proba(x)
+    testing.assert_array_equal((len(x), 2), y_log_proba.shape)
+
+    # test decision_function
+    y_decision = trainer.decision_function(x)
+    testing.assert_array_equal((len(x),), y_decision.shape)
+
+    # test score
+    score = trainer.score(x, y)
+    assert isinstance(score, float), f"Score should be a float, got '{type(score)}' instead"
+
+    # test unsupervised models
+    trainer = trainer.set_params(estimator=PCA(n_components=2, random_state=0), param_grid={})
+    trainer.fit(x, factors=factors, groups=domains)
+
+    # test score_sample
+    score = trainer.score_samples(x)
+    testing.assert_array_equal((len(x),), score.shape)
+
+    # test transform
+    transform = trainer.transform(x, factors=factors)
+    testing.assert_array_equal((len(x), 2), transform.shape)
+
+
+def test_performance_mida_trainer(toy_data):
+    x, y, domains, factors = toy_data
+
+    baseline_trainer = GridSearchCV(
+        estimator=Pipeline(
+            [("scaler", MinMaxScaler()), ("classifier", LogisticRegression(random_state=0, max_iter=10))]
+        ),
+        param_grid={"classifier__C": [2**-5, 1, 2**15], "scaler__feature_range": [(-1, 1), (0, 1)]},
+        error_score="raise",
+        cv=LeaveOneGroupOut(),
+        scoring="roc_auc",
+    )
+
+    mida_trainer = MIDATrainer(
+        estimator=LogisticRegression(random_state=0, max_iter=10),
+        transformer=MinMaxScaler(),
+        param_grid={
+            "C": [2**-5, 1, 2**15],
+            "domain_adapter__mu": [2**-5, 1, 2**15],
+            "transformer__feature_range": [(-1, 1), (0, 1)],
+        },
+        error_score="raise",
+        cv=LeaveOneGroupOut(),
+        scoring="roc_auc",
+    )
+
+    baseline_trainer.fit(x, y, groups=domains)
+    mida_trainer.fit(x, y, factors=factors, groups=domains)
+
+    score_baseline = baseline_trainer.best_score_
+    score_mida = mida_trainer.best_score_
+
+    # Sanity check
+    assert (
+        score_mida > score_baseline
+    ), f"MIDA trainer score ({score_mida:.4f}) is not greater than baseline ({score_baseline:.4f})"
