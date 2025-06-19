063d8af3a56cb8df1d1b1c5f3db112dfb96801d5
Sina Tabakhi <sina.tabakhi@gmail.com>
2025-06-19 20:02:07 +0100

    Add progress bars to feature importance computation

--- kale/interpret/feature_importance.py
+++ kale/interpret/feature_importance.py
@@ -5,6 +5,7 @@
 import numpy as np
 import pandas as pd
 from pytorch_lightning import Trainer
+from tqdm import trange
 
 from kale.loaddata.multiomics_datasets import SparseMultiomicsDataset
 from kale.pipeline.multiomics_trainer import MultiomicsTrainer
@@ -44,14 +45,16 @@ def select_top_features(
     metric_full = test_results[0][metric]
     feat_imp_list = []
 
-    for modality_idx in range(dataset.num_modalities):
+    # for modality_idx in range(dataset.num_modalities):
+    for modality_idx in trange(dataset.num_modalities, desc="Modalities"):
         modality_data = dataset.get(modality_idx)
         num_feats = modality_data.x.shape[1]
         feat_names = modality_data.feat_names
         imp_scores = np.zeros(num_feats)
         modality_label = np.full(num_feats, modality_idx, dtype=int)
 
-        for feat_idx in range(num_feats):
+        # for feat_idx in range(num_feats):
+        for feat_idx in trange(num_feats, desc=f"Features (Modality {modality_idx})", leave=False):
             # Mask the feature
             feat_data = modality_data.x[:, feat_idx].clone()
             modality_data.x[:, feat_idx] = 0
