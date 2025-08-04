5b3a6c36f83c2956703c2925fe750ff250c2f314
Ji Zhongwei <434874154@qq.com>
2025-08-04 21:26:20 +0800

    Fix circular import issue

--- examples/landmark_uncertainty/main.py
+++ examples/landmark_uncertainty/main.py
@@ -26,8 +26,6 @@ from kale.embed.uncertainty_fitting import fit_and_predict
 from kale.interpret.uncertainty_quantiles import generate_fig_comparing_bins, generate_fig_individual_bin_comparison
 from kale.utils.download import download_file_by_url
 
-warnings.filterwarnings("error")
-
 
 def arg_parse():
     """Parsing arguments"""
@@ -124,10 +122,10 @@ def main():
                 for landmark in landmarks:
                     # Define Paths for this loop
                     landmark_results_path_val = os.path.join(
-                        cfg.DATASET.ROOT, base_dir, model, dataset, uncertainty_pairs_val + "_l" + str(landmark)
+                        cfg.DATASET.ROOT, base_dir, model, dataset, uncertainty_pairs_val + "_t" + str(landmark)
                     )
                     landmark_results_path_test = os.path.join(
-                        cfg.DATASET.ROOT, base_dir, model, dataset, uncertainty_pairs_test + "_l" + str(landmark)
+                        cfg.DATASET.ROOT, base_dir, model, dataset, uncertainty_pairs_test + "_t" + str(landmark)
                     )
 
                     fitted_save_at = os.path.join(save_folder, "fitted_quantile_binning", model, dataset)
--- kale/evaluate/similarity_metrics.py
+++ kale/evaluate/similarity_metrics.py
@@ -11,6 +11,7 @@ from typing import Any, Dict, List, Optional, Tuple
 import numpy as np
 import pandas as pd
 
+from kale.interpret.correlation_analysis import fit_line_with_ci
 from kale.prepdata.tabular_transform import apply_confidence_inversion
 
 
@@ -94,7 +95,6 @@ def evaluate_correlations(
         The "all_folds" key contains the correlation statistics for all testing folds combined.
         The "quantiles" key contains the correlation statistics for each quantile bin separately.
     """
-    from kale.interpret.uncertainty_quantiles import fit_line_with_ci
 
     logger = logging.getLogger("qbin")
     # define dict to save correlations to.
