13c4b2f9368f6df0631e513581bd94c6031c815f
Shuo Zhou <shuo.zhou@sheffield.ac.uk>
2025-08-10 01:13:47 +0800

    improve gripnet test coverage

--- kale/prepdata/graph_negative_sampling.py
+++ kale/prepdata/graph_negative_sampling.py
@@ -17,12 +17,12 @@ def negative_sampling(pos_edge_index: torch.Tensor, num_nodes: int) -> torch.Ten
     idx = pos_edge_index[0] * num_nodes + pos_edge_index[1]
     idx = idx.to(torch.device("cpu"))
 
-    perm = torch.tensor(np.random.choice(num_nodes**2, idx.size(0)))
-    mask = torch.from_numpy(np.isin(perm, idx).astype(np.uint8))
+    perm = torch.tensor(np.random.choice(num_nodes**2, idx.size(0)).tolist(), dtype=torch.int)
+    mask = torch.isin(perm, idx)
     rest = mask.nonzero().view(-1)
     while rest.numel() > 0:  # pragma: no cover
-        tmp = torch.tensor(np.random.choice(num_nodes**2, rest.size(0)))
-        mask = torch.from_numpy(np.isin(tmp, idx).astype(np.uint8))
+        tmp = torch.tensor(np.random.choice(num_nodes**2, rest.size(0)).tolist(), dtype=torch.int)
+        mask = torch.isin(tmp, idx)
         perm[rest] = tmp
         rest = mask.nonzero().view(-1)
 
@@ -34,8 +34,7 @@ def typed_negative_sampling(pos_edge_index: torch.Tensor, num_nodes: int, range_
     r"""
     Typed negative sampling for link prediction. Copy-paste from https://github.com/NYXFLOWER/GripNet.
 
-    Args:
-        pos_edge_index (torch.Tensor): edge indices in COO format with shape [2, num_edges].
+    Args:        pos_edge_index (torch.Tensor): edge indices in COO format with shape [2, num_edges].
         num_nodes (int): the number of nodes in the graph.
         range_list (torch.Tensor): the range of edge types. [[start_index, end_index], ...]
 
